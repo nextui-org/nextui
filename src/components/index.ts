9539e7d87ac767bceaa5e5b78f6b75a5091c1274
jrgarciadev <jrgarciadev@gmail.com>
2021-04-23 23:30:45 -0300

    chore: remove rollup by microbundle

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as CssBaseline } from './css-baseline';
-export { default as Checkbox } from './checkbox';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as CssBaseline } from './components/css-baseline';
+export { default as Checkbox } from './components/checkbox';
