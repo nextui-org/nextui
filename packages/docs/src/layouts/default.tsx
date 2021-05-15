7dbe418e03304e6475c1fa1afcc377ea73d8d2f7
Junior García <jrgarciadev@gmail.com>
2021-05-15 19:09:32 -0300

    feat: logo component added

--- packages/docs/src/layouts/default.tsx
+++ packages/docs/src/layouts/default.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BaseLayout from './base';
 import Footer from './footer';
+import Navbar from './navbar';
 
 const DefaultLayout: React.FC<React.PropsWithChildren<unknown>> = ({
   children,
 }) => {
   return (
     <BaseLayout>
+      <Navbar />
       {children}
       <Footer />
     </BaseLayout>
---
+++ packages/docs/src/layouts/navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Logo } from '@components';
+
+const Navbar: React.FC = () => {
+  return <Logo />;
+};
+
+export default Navbar;
