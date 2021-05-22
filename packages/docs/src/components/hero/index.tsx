7b34451de89813fbb0665f01838ed765ac63f5e8
Junior García <jrgarciadev@gmail.com>
2021-05-21 23:10:29 -0300

    feat: lading page improved

--- packages/docs/src/components/hero/index.tsx
+++ packages/docs/src/components/hero/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Row, Col } from '@nextui/react';
+import { Text, Row, Col, Grid, Spacer, Button, Image } from '@nextui/react';
 import { ImageBrowser } from '@components';
 
 const Hero: React.FC = () => {
@@ -7,12 +7,57 @@ const Hero: React.FC = () => {
     <>
       <Row className="hero-container">
         <Col span={6}>
-          <Text h1 className="hero__title" size="4.2rem">
+          <Text h1 className="hero__title" size="4rem">
             Beautiful, fast and
           </Text>
-          <Text h1 size="4.2rem" className="hero__title hero__title-smooth">
+          <Text h1 size="4rem" className="hero__title hero__title-smooth">
             modern React UI library.
           </Text>
+          <Spacer y={1.5} />
+          <Grid.Container gap={2} justify="center">
+            <Grid xs={6}>
+              <Row align="middle">
+                <img src="/responsive.svg" width="24" alt="responsive icon" />
+                <Spacer />
+                <Text b size="1.1rem">
+                  Responsive design
+                </Text>
+              </Row>
+            </Grid>
+            <Grid xs={6}>
+              <Row align="middle">
+                <img src="/components.svg" width="24" alt="components icon" />
+                <Spacer />
+                <Text b size="1.1rem">
+                  +100 Components
+                </Text>
+              </Row>
+            </Grid>
+            <Grid xs={6}>
+              <Row align="middle">
+                <img src="/graph.svg" width="24" alt="graph icon" />
+                <Spacer />
+                <Text b size="1.1rem">
+                  Graph & analytics
+                </Text>
+              </Row>
+            </Grid>
+            <Grid xs={6}>
+              <Row align="middle">
+                <img src="/open_source.svg" width="24" alt="open source icon" />
+                <Spacer />
+                <Text b size="1.1rem">
+                  Open source
+                </Text>
+              </Row>
+            </Grid>
+          </Grid.Container>
+          <Spacer y={2} />
+          <Row align="middle">
+            <Button>Get Started</Button>
+            <Spacer x={1} />
+            <Button light>View on Github</Button>
+          </Row>
         </Col>
         <Col span={6} className="hero__image-container">
           <ImageBrowser className="hero__image" />
@@ -20,7 +65,7 @@ const Hero: React.FC = () => {
       </Row>
       <style jsx>{`
         :global(.hero-container) {
-          padding: 20vh 0;
+          padding: 14vh 0;
         }
         :global(.hero__title) {
           margin-bottom: 0rem;
