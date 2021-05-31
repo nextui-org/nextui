661a9cee89f9d27911de7118587607432d9a5b38
Junior García <jrgarciadev@gmail.com>
2021-05-30 22:22:26 -0300

    chore: mdx installed and configured

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,5 +1,27 @@
-module.exports = {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [require('@mapbox/rehype-prism')],
+  },
+});
+module.exports = withMDX({
   future: {
     webpack5: true,
   },
-};
+  pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
+
+  async redirects() {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/guide/introduction',
+        permanent: true,
+      },
+      {
+        source: '/guide',
+        destination: '/docs/guide/introduction',
+        permanent: true,
+      },
+    ];
+  },
+});
