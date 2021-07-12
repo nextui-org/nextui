a2593d4d961c1a453923cf1e42d455088924e5e1
Junior García <jrgarciadev@gmail.com>
2021-07-12 16:55:30 -0300

    chore: unnecesarry changes

--- packages/docs/src/components/palette/index.tsx
+++ packages/docs/src/components/palette/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { useTheme, NextUIThemes } from '@nextui-org/react';
+import { useTheme, NextUIThemes, Grid } from '@nextui-org/react';
 import { get, replace } from 'lodash';
 import { invertHex, hexToRGBA, hexFromString } from '@utils/index';
 
@@ -10,8 +10,9 @@ export interface Props {
 
 const Palette: React.FC<Props> = ({ colors, inverted }) => {
   const theme = useTheme() as NextUIThemes;
+
   const renderItem = useCallback(
-    (color, index) => {
+    (color: string) => {
       const hexColor = get(theme.palette, color || 'primary');
       const textColor = inverted ? invertHex(hexColor) : theme.palette.white;
       const shadowColor =
@@ -20,31 +21,39 @@ const Palette: React.FC<Props> = ({ colors, inverted }) => {
           : get(theme.palette, color || 'primary');
 
       return (
-        <div key={index} className="color">
+        <div key={color} className="color">
           <p className="text">{replace(color, '_', ' ')}</p>
           <style jsx>{`
             .color {
               background: ${hexColor};
-              margin-right: 20px;
               margin-bottom: 20px;
               width: 100px;
               height: 100px;
               display: flex;
-              text-transform: capitalize;
               justify-content: center;
               align-items: center;
               box-shadow: 0 20px 35px -10px ${hexToRGBA(shadowColor, 0.5)};
               border-radius: ${theme.layout.radius};
               transition: all 0.25s ease;
             }
-            .color:hover {
+            .color:hover) {
               transform: translateY(5px);
               box-shadow: 0 0 0 0 ${hexToRGBA(shadowColor, 0.5)};
             }
             .text {
               color: ${textColor};
               font-weight: bold;
+              text-transform: capitalize;
               font-size: 13px;
+            }
+            @media only screen and (max-width: ${theme.breakpoints.sm.min}) {
+              .color {
+                width: 80px;
+                height: 80px;
+              }
+              .text {
+                font-size: 10px;
+              }
             }
           `}</style>
         </div>
@@ -58,8 +67,13 @@ const Palette: React.FC<Props> = ({ colors, inverted }) => {
       {colors?.map(renderItem)}
       <style jsx>{`
         .palette {
+          --grid-gap-unit: calc(2 * 4pt);
           display: flex;
           flex-wrap: wrap;
+          box-sizing: border-box;
+          grid-gap: calc(2 * 4pt);
+          margin: calc(-1 * var(--grid-gap-unit));
+          width: calc(100% + var(--grid-gap-unit) * 2);
           padding-bottom: ${theme.layout.gapHalf};
         }
       `}</style>
