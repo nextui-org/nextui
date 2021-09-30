1b7f04b7f27c7d9774c6be544fa8ec1f515281bc
Junior García <jrgarciadev@gmail.com>
2021-09-29 23:43:36 -0300

    fix: pre-build script, version increased

--- packages/nextui/scripts/setup-package.js
+++ packages/nextui/scripts/setup-package.js
@@ -10,21 +10,24 @@ function main() {
   sourceObj.scripts = {};
   sourceObj.devDependencies = {};
 
-  //   if (sourceObj.main.startsWith('lib/')) {
-  //     sourceObj.main = sourceObj.main.replace('lib/', '');
-  //   }
-  //   if (sourceObj.module.startsWith('lib/')) {
-  //     sourceObj.module = sourceObj.module.replace('lib/', '');
-  //   }
-  //   if (sourceObj.unpkg.startsWith('lib/')) {
-  //     sourceObj.unpkg = sourceObj.unpkg.replace('lib/', '');
-  //   }
-  //   if (sourceObj.types.startsWith('lib/')) {
-  //     sourceObj.types = sourceObj.types.replace('lib/', '');
-  //   }
-  //   if (sourceObj.publishConfig) {
-  //     delete sourceObj.publishConfig;
-  //   }
+  if (sourceObj.main.startsWith('lib/')) {
+    sourceObj.main = sourceObj.main.replace('lib/', '');
+  }
+  if (sourceObj.module.startsWith('lib/')) {
+    sourceObj.module = sourceObj.module.replace('lib/', '');
+  }
+  if (sourceObj.unpkg.startsWith('lib/')) {
+    sourceObj.unpkg = sourceObj.unpkg.replace('lib/', '');
+  }
+  if (sourceObj.types.startsWith('lib/')) {
+    sourceObj.types = sourceObj.types.replace('lib/', '');
+  }
+  if (sourceObj.publishConfig) {
+    delete sourceObj.publishConfig;
+  }
+  if (sourceObj.files) {
+    delete sourceObj.files;
+  }
   fs.writeFileSync(
     libRoot + '/package.json',
     Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8')
