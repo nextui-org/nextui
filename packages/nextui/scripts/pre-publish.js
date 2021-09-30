2e9251c7c844072df27d2407fce55fd552676782
Junior García <jrgarciadev@gmail.com>
2021-09-29 23:13:13 -0300

    feat: pre publish script improved

--- packages/nextui/scripts/pre-publish.js
+++ packages/nextui/scripts/pre-publish.js
@@ -1,17 +1,24 @@
 const setupPackage = require('./setup-package');
-
+const path = require('path');
+const fs = require('fs-extra');
 const shell = require('./utils').shell;
 const step = require('./utils').step;
 const error = require('./utils').error;
+const libRoot = path.join(__dirname, '../lib');
 
-const buildPkg = step('building...', () => shell(`yarn build`));
+const buildPkg = step('build pkg...', () => shell(`yarn build`));
+
+const printPkg = step('print pkg...', () => {
+  const genPkgJson = fs
+    .readFileSync(`${libRoot}/package.json`)
+    .toString('utf-8');
+  console.log(JSON.parse(genPkgJson));
+});
 
 Promise.resolve(true)
   .then(buildPkg)
-  .then(
-    Promise.all([setupPackage()]).then(() => {
-      console.log(process.cwd());
-      //   shell('pwd');
-    })
-  )
+  .then(() => {
+    setupPackage();
+    printPkg();
+  })
   .catch(error);
