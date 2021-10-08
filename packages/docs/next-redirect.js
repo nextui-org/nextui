04fd908210a1dad4d6cb225cda890d0459001767
Junior García <jrgarciadev@gmail.com>
2021-10-08 20:44:05 -0300

    feat: components redirects added, documentation improved

--- packages/docs/next-redirect.js
+++ packages/docs/next-redirect.js
@@ -1,5 +1,33 @@
+const shell = require('shelljs');
+const path = require('path');
+
+const rootDir = path.join(__dirname, '.');
+const contentDir = path.join(rootDir, 'content');
+const docsDir = path.join(contentDir, 'docs');
+const componentsDocsDir = path.join(docsDir, 'components');
+
+const getComponentsName = () => {
+  const names = shell
+    .ls('-R', componentsDocsDir)
+    .map((file) => path.join(process.cwd(), componentsDocsDir, file))
+    .filter((file) => file.endsWith('.mdx'))
+    .map((file) => path.basename(file, '.mdx'));
+  return names;
+};
+const getComponentsRoute = (names = []) => {
+  return names.map((name) => {
+    return {
+      source: `/${name}`,
+      destination: `/docs/components/${name}`,
+      permanent: true,
+    };
+  });
+};
+
 async function redirect() {
+  const componentsName = getComponentsName();
   return [
+    ...getComponentsRoute(componentsName),
     {
       source: '/docs',
       destination: '/docs/guide/getting-started',
