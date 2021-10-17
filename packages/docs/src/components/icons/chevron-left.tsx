ef7a30691a31c2795cde94795b12c3866b094919
Junior García <jrgarciadev@gmail.com>
2021-10-16 22:01:54 -0300

    chore(docs): new icons added and generic component for using icons

---
+++ packages/docs/src/components/icons/chevron-left.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+export interface Props {
+  fill?: string;
+  width?: number;
+  height?: number;
+  size?: number;
+}
+
+const ChevronLeft: React.FC<Props> = ({
+  fill,
+  size,
+  width = 24,
+  height = 24,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size || width}
+      height={size || height}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke={fill}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+      >
+        <path d="M9.57 5.93L3.5 12l6.07 6.07" />
+        <path d="M20.5 12H3.67" />
+      </g>
+    </svg>
+  );
+};
+
+export default ChevronLeft;
