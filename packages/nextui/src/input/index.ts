0c5009b3f511f039770e3ab2df317effc49b7fc1
Junior García <jrgarciadev@gmail.com>
2021-11-06 15:51:59 -0300

    fix(inpu): focus styles removed

--- packages/nextui/src/input/index.ts
+++ packages/nextui/src/input/index.ts
@@ -1,6 +1,9 @@
+import { FormElement } from './input-props';
 import Input from './input';
 import Textarea from '../textarea';
 import InputPassword from './input-password';
+
+export type { FormElement };
 
 Input.Textarea = Textarea;
 Input.Password = InputPassword;
