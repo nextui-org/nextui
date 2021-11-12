f2fe33d026428a0e9e5ca5503f483008f834facf
Junior García <jrgarciadev@gmail.com>
2021-11-11 22:14:07 -0300

    fix: carbon space reserved to avoid CLS

--- packages/docs/src/components/carbon-ad/index.tsx
+++ packages/docs/src/components/carbon-ad/index.tsx
@@ -30,96 +30,101 @@ const CarbonAd: React.FC<unknown> = () => {
   if (!isProd) return null;
 
   return (
-    <span id="carbon-ad" ref={ref}>
-      <style jsx global>
-        {`
-          #carbonads * {
-            margin: initial;
-            padding: initial;
-          }
-          #carbonads {
-            font-family: inherit;
-            max-width: 100%;
-            padding: ${theme.spacing.md};
-          }
-          #carbonads {
-            display: flex;
-            width: 100%;
-            z-index: 100;
-            border-radius: ${theme.radius.lg};
-            background-color: ${theme.type === 'light' ? '#363449' : '#111'};
-            box-shadow: 0px 5px 20px -5px rgb(0 0 0 / 20%);
-          }
-          #carbonads a {
-            color: inherit;
-            text-decoration: none;
-            transition: all 0.25s ease;
-          }
-          #carbonads a:hover {
-            color: ${theme.type === 'light'
-              ? theme.palette.accents_3
-              : theme.palette.accents_7};
-          }
-          #carbonads span {
-            width: 100%;
-            position: relative;
-            display: block;
-            overflow: hidden;
-          }
-          #carbonads .carbon-wrap {
-            display: flex;
-            flex-direction: row;
-          }
-          #carbonads .carbon-img {
-            display: block;
-            line-height: 1;
-            max-width: 130px;
-            width: 100%;
-          }
-          #carbonads .carbon-img img {
-            display: block;
-            max-width: 100% !important;
-            width: 100%;
-            border-radius: ${theme.radius.md};
-            border: 0px;
-            margin: 0px;
-          }
-          #carbonads .carbon-text {
-            display: block;
-            font-size: 1rem;
-            width: 100%;
-            color: ${theme.type === 'light'
-              ? theme.palette.accents_2
-              : theme.palette.accents_6};
-            padding-left: ${theme.spacing.md};
-            padding-right: ${theme.spacing.md};
-          }
-          #carbonads .carbon-poweredby {
-            display: flex;
-            justify-content: flex-end;
-            align-items: center;
-            position: absolute;
-            bottom: 0;
-            right: 0;
-            padding: 10px 0;
-            color: ${theme.type === 'light'
-              ? theme.palette.accents_1
-              : theme.palette.accents_4};
-            text-transform: uppercase;
-            letter-spacing: 0.5px;
-            font-weight: 600;
-            font-size: 9px;
-            line-height: 0;
-            transition: all 0.25 ease;
-          }
-          @media only screen and (max-width: ${theme.breakpoints.xs}) {
-            #carbonads .carbon-text {
-              font-size: 0.9rem;
+    <div className="carbon-ad-container">
+      <span id="carbon-ad" ref={ref}>
+        <style jsx global>
+          {`
+            .carbon-ad-container {
+              min-height: 132px;
             }
-          }
-        `}
-      </style>
-    </span>
+            #carbonads * {
+              margin: initial;
+              padding: initial;
+            }
+            #carbonads {
+              font-family: inherit;
+              max-width: 100%;
+              padding: ${theme.spacing.md};
+            }
+            #carbonads {
+              display: flex;
+              width: 100%;
+              z-index: 100;
+              border-radius: ${theme.radius.lg};
+              background-color: ${theme.type === 'light' ? '#363449' : '#111'};
+              box-shadow: 0px 5px 20px -5px rgb(0 0 0 / 20%);
+            }
+            #carbonads a {
+              color: inherit;
+              text-decoration: none;
+              transition: all 0.25s ease;
+            }
+            #carbonads a:hover {
+              color: ${theme.type === 'light'
+                ? theme.palette.accents_3
+                : theme.palette.accents_7};
+            }
+            #carbonads span {
+              width: 100%;
+              position: relative;
+              display: block;
+              overflow: hidden;
+            }
+            #carbonads .carbon-wrap {
+              display: flex;
+              flex-direction: row;
+            }
+            #carbonads .carbon-img {
+              display: block;
+              line-height: 1;
+              max-width: 130px;
+              width: 100%;
+            }
+            #carbonads .carbon-img img {
+              display: block;
+              max-width: 100% !important;
+              width: 100%;
+              border-radius: ${theme.radius.md};
+              border: 0px;
+              margin: 0px;
+            }
+            #carbonads .carbon-text {
+              display: block;
+              font-size: 1rem;
+              width: 100%;
+              color: ${theme.type === 'light'
+                ? theme.palette.accents_2
+                : theme.palette.accents_6};
+              padding-left: ${theme.spacing.md};
+              padding-right: ${theme.spacing.md};
+            }
+            #carbonads .carbon-poweredby {
+              display: flex;
+              justify-content: flex-end;
+              align-items: center;
+              position: absolute;
+              bottom: 0;
+              right: 0;
+              padding: 10px 0;
+              color: ${theme.type === 'light'
+                ? theme.palette.accents_1
+                : theme.palette.accents_4};
+              text-transform: uppercase;
+              letter-spacing: 0.5px;
+              font-weight: 600;
+              font-size: 9px;
+              line-height: 0;
+              transition: all 0.25 ease;
+            }
+            @media only screen and (max-width: ${theme.breakpoints.xs}) {
+              #carbonads .carbon-text {
+                font-size: 0.9rem;
+              }
+            }
+          `}
+        </style>
+      </span>
+    </div>
   );
 };
 
