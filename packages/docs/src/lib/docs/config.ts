5614696b68dec21ac9c9b676f564db03a4151a03
Junior García <jrgarciadev@gmail.com>
2021-12-26 17:53:01 -0300

    chore(docs): tag branch returned to main

--- packages/docs/src/lib/docs/config.ts
+++ packages/docs/src/lib/docs/config.ts
@@ -1,5 +1,5 @@
 // Tag to use if FORCE_TAG is enabled (only for production build)
-export const TAG = 'test/stitches-as-css-in-js'; // Github branch
+export const TAG = 'main'; // Github branch
 
 export const CONTENT_PATH = '/packages/docs/content';
 
