2937c1ca72863512ae936e61ba0a2b1818e31e9f
Junior García <jrgarciadev@gmail.com>
2022-01-01 23:12:20 -0300

    chore: build working

---
+++ packages/storybook/storybook-static/5.03ad87ec.iframe.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],[,,function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){},function(module,exports){}]]);
