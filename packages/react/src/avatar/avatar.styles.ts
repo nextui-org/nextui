7dd5fef228a1c27fee666c164c6478f3ab10f097
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-02-24 18:46:38 +0530

    Readded Old Code Fragments

--- packages/react/src/avatar/avatar.styles.ts
+++ packages/react/src/avatar/avatar.styles.ts
@@ -29,6 +29,13 @@ export const StyledAvatar = styled('span', {
     opacity: 1
   },
   '.nextui-avatar-bg': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: '$2',
+    transition: '$default',
     size: '100%'
   },
   '.nextui-avatar-icon': {
