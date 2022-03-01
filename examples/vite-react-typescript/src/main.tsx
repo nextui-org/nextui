e2c0ff57fff55ed81ef3e503ff341558e3488ed9
Junior García <jrgarciadev@gmail.com>
2022-03-01 10:58:34 -0300

    chore(examples): vite example modified

--- examples/vite-react-typescript/src/main.tsx
+++ examples/vite-react-typescript/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
