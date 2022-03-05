3be6a095150e65dca7b40416cef6a21d122d2b9e
Junior García <jrgarciadev@gmail.com>
2022-03-05 10:40:08 -0300

    fix[docs](env): new enviroment variable added to disable typescript checks

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
   redirects: require('./next-redirect'),
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
+  typescript: {
+    ignoreBuildErrors: process.env.IS_VERCEL_ENV === 'true'
+  }
 };
