3978271e0033ec9faf9ac667cb5847de9bc70742
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-03-15 13:45:52 +0530

    Avatar Icon Done

---
+++ apps/docs/content/components/avatar/index.ts
@@ -0,0 +1,17 @@
+import default_avatar from './default';
+import size from './size';
+import color from './color'; 
+import bordered from './bordered';
+import zoomed from './zoomed';
+import squared from './squared';
+import icon from './icon';
+
+export default {
+  default_avatar,
+  size,
+  color,
+  bordered,
+  zoomed,
+  squared,
+  icon
+};
--- apps/docs/content/components/index.ts
+++ apps/docs/content/components/index.ts
@@ -1,9 +1,11 @@
 import tableContent from './table';
 import UserContent from './user';
 import imageContent from './image';
+import avatarContent from './avatar';
 
 export default {
   tableContent,
   UserContent,
-  imageContent
+  imageContent,
+  avatarContent
 };
