a8dfd7e282be646b0e444ea009167103e9c59bdd
Junior Garcia <jrgarciadev@gmail.com>
2022-03-18 16:21:53 -0300

    chore[docs](avatar): format changed

--- apps/docs/content/components/avatar/default.ts
+++ apps/docs/content/components/avatar/default.ts
@@ -1,28 +1,29 @@
-const App = `import { Avatar,Grid } from '@nextui-org/react';
+const App = `import { Avatar, Grid } from '@nextui-org/react';
     
 export default function App() {
-  return <Grid.Container gap={2}>
-  <Grid>
-      <Avatar squared src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-  </Grid>
-  <Grid>
-      <Avatar squared text="Junior" />
-  </Grid>
-  <Grid>
-      <Avatar squared src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-  </Grid>
-  <Grid>
-      <Avatar squared text="Jane" />
-  </Grid>
-  <Grid>
-      <Avatar squared src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-  </Grid>
-  <Grid>
-      <Avatar squared text="Joe" />
-  </Grid>
-</Grid.Container>
-}
-`;
+  return (
+    <Grid.Container gap={2}>
+      <Grid>
+        <Avatar squared src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+      </Grid>
+      <Grid>
+        <Avatar squared text="Junior" />
+      </Grid>
+      <Grid>
+        <Avatar squared src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+      </Grid>
+      <Grid>
+        <Avatar squared text="Jane" />
+      </Grid>
+      <Grid>
+        <Avatar squared src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+      </Grid>
+      <Grid>
+        <Avatar squared text="Joe" />
+      </Grid>
+    </Grid.Container>
+  );
+};`;
 
 const react = {
   '/App.js': App
