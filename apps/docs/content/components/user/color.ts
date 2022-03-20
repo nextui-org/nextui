44fac63d321cec76cbcfff2db71f399a7fe8e689
Junior Garcia <jrgarciadev@gmail.com>
2022-03-20 19:33:23 -0300

    chore[docs](user): some fixes

--- apps/docs/content/components/user/color.ts
+++ apps/docs/content/components/user/color.ts
@@ -6,41 +6,41 @@ export default function App() {
       <Grid.Container gap={2}>
         <Grid>
           <User
+            bordered
             src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
             name="Ariana Wattson"
-            bordered
             color="primary"
           />
         </Grid>
         <Grid>
           <User
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            name="Ariana Wattson"
             bordered
+            src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+            name="Jane Fisher"
             color="secondary"
           />
         </Grid>
         <Grid>
           <User
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            name="Ariana Wattson"
             bordered
+            src="https://i.pravatar.cc/150?u=a048581f4e29026701d"
+            name="William Howard"
             color="success"
           />
         </Grid>
         <Grid>
-          <User
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            name="Ariana Wattson"
+        <User
             bordered
+            src="https://i.pravatar.cc/150?u=a092581d4ef9026700d"
+            name="Kristen Copper"
             color="warning"
           />
         </Grid>
         <Grid>
           <User
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            name="Ariana Wattson"
             bordered
+            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+            name="Tony Reichert"
             color="error"
           />
         </Grid>
--- apps/docs/content/components/user/size.ts
+++ apps/docs/content/components/user/size.ts
@@ -10,26 +10,26 @@ export default function App() {
       />
       <Spacer />
       <User
-        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        name="Ariana Wattson"
+        src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+        name="Jane Fisher"
         size="sm"
       />
       <Spacer />
       <User
-        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        name="Ariana Wattson"
+        src="https://i.pravatar.cc/150?u=a048581f4e29026701d"
+        name="William Howard"
         size="md"
       />
       <Spacer />
       <User
-        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        name="Ariana Wattson"
+        src="https://i.pravatar.cc/150?u=a092581d4ef9026700d"
+        name="Kristen Copper"
         size="lg"
       />
       <Spacer />
       <User
-        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        name="Ariana Wattson"
+        src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+        name="Tony Reichert"
         size="xl"
       />
     </>
