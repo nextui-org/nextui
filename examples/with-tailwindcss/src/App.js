d58d2653cfc4f14c99cdd9452979a684dcf57bd6
Junior Garcia <jrgarciadev@gmail.com>
2022-04-24 18:05:50 -0300

    feat(exmples): tailwindcss example added

---
+++ examples/with-tailwindcss/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Container, Link, Button } from '@nextui-org/react';
+
+function App() {
+  return (
+    <Container className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-7xl mb-10 font-bold">
+        Welcome to&nbsp;
+        <Link href="https://tailwindcss.com/" rel="noreferer" target="_blank">
+          tailwindcss
+        </Link>
+        &nbsp;+&nbsp;
+        <Link
+          href="https://nextui.org?utm_source=with-tailwindcss-example"
+          target="_blank"
+        >
+          NextUI
+        </Link>
+      </h1>
+      <Button
+        href="https://nextui.org?utm_source=with-tailwindcss-example"
+        target="_blank"
+        as="a"
+      >
+        Go to NextUI
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
