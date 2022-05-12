cc692d272c2099163e2a3f9262b9d2b1a9240c08
TianenPang <32772271+TianenPang@users.noreply.github.com>
2022-05-13 00:01:39 +0800

    fix(docs): initial style elements are missing key

--- apps/docs/src/pages/_document.tsx
+++ apps/docs/src/pages/_document.tsx
@@ -16,12 +16,7 @@ class MyDocument extends Document {
 
     return {
       ...initialProps,
-      styles: [
-        <>
-          {initialProps.styles}
-          {styledJSXStyles}
-        </>
-      ]
+      styles: React.Children.toArray([initialProps.styles, styledJSXStyles])
     };
   }
   render() {
