6d8743647987440bf4bd501746cc7711ecc573eb
Junior Garcia <jrgarciadev@gmail.com>
2022-05-29 18:25:36 -0300

    chore(react-dropdown): types and variants exported

--- packages/react/src/dropdown/index.ts
+++ packages/react/src/dropdown/index.ts
@@ -11,4 +11,30 @@ Dropdown.Section = Section;
 Dropdown.Item = DropdownItemBase;
 Dropdown.Button = DropdownButton;
 
+// export styled components
+export {
+  StyledDropdownMenu,
+  StyledDropdownItem,
+  StyledDropdownItemIconWrapper,
+  StyledDropdownItemKbd,
+  StyledDropdownItemContentWrapper,
+  StyledDropdownItemContent,
+  StyledDropdownItemDescription,
+  StyledDropdownSectionWrapper,
+  StyledDropdownSection,
+  StyledDropdownSectionTitle
+} from './dropdown.styles';
+
+// export types
+export type {
+  DropdownItemVariantsProps,
+  StyledDropdownItemDescriptionProps
+} from './dropdown.styles';
+
+export type { DropdownProps } from './dropdown';
+export type { DropdownMenuProps } from './dropdown-menu';
+export type { DropdownItemProps } from './dropdown-item';
+export type { DropdownButtonProps } from './dropdown-button';
+export type { DropdownSectionProps } from './dropdown-section';
+
 export default Dropdown;
