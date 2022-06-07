a23888777bddf3485bcefc4b1acf2669e9fc7428
TianenPang <32772271+TianenPang@users.noreply.github.com>
2022-06-07 08:02:19 +0800

    fix(docs-layout): wrapped card in card.body and fixed the background color

--- apps/docs/content/components/container/default.ts
+++ apps/docs/content/components/container/default.ts
@@ -3,14 +3,16 @@ const App = `import { Container, Card, Row, Text } from "@nextui-org/react";
 export default function App() {
   return (
     <Container>
-      <Card color="primary">
-        <Row justify="center" align="center">
-          <Text h6 size={15} color="white" css={{ m: 0 }}>
-            NextUI gives you the best developer experience with all the features
-            you need for building beautiful and modern websites and
-            applications.
-          </Text>
-        </Row>
+      <Card css={{ $$cardColor: '$colors$primary' }}>
+        <Card.Body>
+          <Row justify="center" align="center">
+            <Text h6 size={15} color="white" css={{ m: 0 }}>
+              NextUI gives you the best developer experience with all the features
+              you need for building beautiful and modern websites and
+              applications.
+            </Text>
+          </Row>
+        </Card.Body>
       </Card>
     </Container>
   );
