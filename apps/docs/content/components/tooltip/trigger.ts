fdf03d8cd2f3a948f2b4c8d748554ede52fb6a03
Chell <im.chell@outlook.com>
2022-06-10 17:51:32 +0800

    format trigger

--- apps/docs/content/components/tooltip/trigger.ts
+++ apps/docs/content/components/tooltip/trigger.ts
@@ -1,20 +1,22 @@
-const App = `import { Tooltip, Grid, Link } from "@nextui-org/react";
+const App = `import { Tooltip, Grid, Link, Card, Text } from "@nextui-org/react";
 
 export default function App() {
   return (
     <Grid.Container gap={2}>
       <Grid>
-        <Tooltip
-          content={"Developers love Next.js"}
-          trigger="click"
-          color="primary"
-        >
         <Card variant="flat">
-          <Link>
-            Click me
-          </Link>
+          <Card.Body>
+            <Tooltip
+              content={"Developers love Next.js"}
+              trigger="click"
+              color="primary"
+            >
+              <Link>
+                Click me
+              </Link>
+            </Tooltip>
+          </Card.Body>
         </Card>
-        </Tooltip>
       </Grid>
       <Grid>
         <Tooltip
