a7192d41b2df90611f02b9e269a259ef51d4b503
태재영 <tjy970721@gmail.com>
2022-07-06 05:43:48 +0900

    docs: refactor modal

--- apps/docs/content/components/modal/blurBackground.ts
+++ apps/docs/content/components/modal/blurBackground.ts
@@ -36,13 +36,13 @@ const Mail = `export const Mail = ({ fill, size, height, width, ...props }) => {
   );
 };`;
 
-const AppJs = `import React from "react";
+const AppJs = `import { useState } from "react";
 import { Modal, Input, Row, Checkbox, Button, Text } from "@nextui-org/react";
 import { Mail } from "./Mail";
 import { Password } from "./Password";
 
 export default function App() {
-  const [visible, setVisible] = React.useState(false);
+  const [visible, setVisible] = useState(false);
   const handler = () => setVisible(true);
   const closeHandler = () => {
     setVisible(false);
