f39d0cb26b26205526ce73ba2acc943f91cf4a10
Junior Garcia <jrgarciadev@gmail.com>
2022-07-31 15:25:42 -0300

    feat(react-navbar): collapse items transition improved

--- packages/react/src/navbar/navbar-collapse.tsx
+++ packages/react/src/navbar/navbar-collapse.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useMemo} from "react";
+import React, {useState, useEffect, useMemo} from "react";
 
 import {HTMLNextUIProps, forwardRef} from "../utils/system";
 import {useDOMRef} from "../utils/dom";
 import clsx from "../utils/clsx";
-import {__DEV__} from "../utils/assertion";
 import useIntersectionObserver from "../use-intersection-observer";
+import {__DEV__} from "../utils/assertion";
 
 import {useNavbarContext} from "./navbar-context";
 import {
@@ -20,10 +20,13 @@ interface Props extends HTMLNextUIProps<"ul"> {}
 export type NavbarCollapseProps = Props & NavbarCollapseVariantsProps;
 
 const NavbarCollapseItem = forwardRef<
-  HTMLNextUIProps<"li"> & NavbarCollapseItemVariantsProps,
+  HTMLNextUIProps<"li"> &
+    NavbarCollapseItemVariantsProps & {
+      transitionDelay?: number; // in seconds
+    },
   "li"
 >((props, ref) => {
-  const {children, className, disableAnimation, style, css, ...otherProps} = props;
+  const {children, className, transitionDelay, disableAnimation, style, css, ...otherProps} = props;
   const context = useNavbarContext();
 
   const domRef = useDOMRef(ref);
@@ -40,16 +43,19 @@ const NavbarCollapseItem = forwardRef<
   }, [isVisible, context.isCollapseOpen]);
 
   const itemStyle = useMemo(() => {
+    const timeDelay = isVisible ? transitionDelay : 0;
+
     return {
       "--nextui--collapseItemOpacity": isVisible ? 1 : 0,
       "--nextui--collapseItemTransform": isVisible
         ? "matrix(1, 0, 0, 1, 0, 0)"
         : "matrix(0.85, 0, 0, 0.85, 5, 20)",
-      "--netxui--collapseItemTransition": !disableAnimation
-        ? "opacity 1s cubic-bezier(0.5, 0, 0, 1) 0s, transform 1s cubic-bezier(0.5, 0, 0, 1) 0s"
-        : "none",
+      "--netxui--collapseItemTransition":
+        !disableAnimation && context.isCollapseOpen
+          ? `opacity 1s cubic-bezier(0.5, 0, 0, 1) ${timeDelay}s, transform 1s cubic-bezier(0.5, 0, 0, 1) ${timeDelay}s`
+          : "none",
     };
-  }, [disableAnimation, isVisible]);
+  }, [disableAnimation, isVisible, transitionDelay, context.isCollapseOpen]);
 
   return (
     <StyledNavbarCollapseItem
@@ -73,6 +79,8 @@ const NavbarCollapseItem = forwardRef<
 const NavbarCollapse = forwardRef<NavbarCollapseProps, "ul">((props, ref) => {
   const {children, className, ...otherProps} = props;
 
+  const [hasScrolled, setHasScrolled] = useState(false);
+
   const context = useNavbarContext();
   const domRef = useDOMRef(ref);
 
@@ -82,7 +90,20 @@ const NavbarCollapse = forwardRef<NavbarCollapseProps, "ul">((props, ref) => {
       domRef.current?.scrollTo({
         top: 0,
       });
+      setHasScrolled(false);
     }
+
+    const handleScroll = () => {
+      if (domRef.current?.scrollTop > 0 && !hasScrolled) {
+        setHasScrolled(true);
+      }
+    };
+
+    domRef.current?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      domRef.current?.removeEventListener("scroll", handleScroll);
+    };
   }, [context.isCollapseOpen]);
 
   const items = [
@@ -114,7 +135,11 @@ const NavbarCollapse = forwardRef<NavbarCollapseProps, "ul">((props, ref) => {
       <StyledNavbarCollapseWrapper className="nextui-navbar-collapse-wrapper">
         {children}
         {items.map((item, index) => (
-          <NavbarCollapseItem key={`${item}-${index}`} className="nextui-navbar-collapse-item">
+          <NavbarCollapseItem
+            key={`${item}-${index}`}
+            className="nextui-navbar-collapse-item"
+            transitionDelay={hasScrolled ? 0 : index / items.length}
+          >
             {item}
           </NavbarCollapseItem>
         ))}
