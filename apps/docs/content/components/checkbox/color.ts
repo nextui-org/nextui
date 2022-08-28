f6c9a7bdb1fb146b252baffc1992cad324ecc785
Junior Garcia <jrgarciadev@gmail.com>
2022-08-28 16:30:03 -0300

    refactor(react-checbox): new prop added to manage the container css, docs updated

--- apps/docs/content/components/checkbox/color.ts
+++ apps/docs/content/components/checkbox/color.ts
@@ -3,27 +3,27 @@ const App = `import { Checkbox, Spacer } from "@nextui-org/react";
 export default function App() {
   return (
     <>
-      <Checkbox color="primary" defaultSelected={true}>
+      <Checkbox color="primary" defaultSelected>
         Primary
       </Checkbox>
       <Spacer />
-      <Checkbox color="secondary" defaultSelected={true}>
+      <Checkbox color="secondary" defaultSelected>
         Secondary
       </Checkbox>
       <Spacer />
-      <Checkbox color="success" defaultSelected={true}>
+      <Checkbox color="success" defaultSelected>
         Success
       </Checkbox>
       <Spacer />
-      <Checkbox color="warning" defaultSelected={true}>
+      <Checkbox color="warning" defaultSelected>
         Warning
       </Checkbox>
       <Spacer />
-      <Checkbox color="error" defaultSelected={true}>
+      <Checkbox color="error" defaultSelected>
         Error
       </Checkbox>
       <Spacer />
-      <Checkbox color="gradient" defaultSelected={true}>
+      <Checkbox color="gradient" defaultSelected>
         Gradient
       </Checkbox>
     </>
--- apps/docs/content/components/checkbox/disabled.ts
+++ apps/docs/content/components/checkbox/disabled.ts
@@ -2,7 +2,7 @@ const App = `import { Checkbox } from "@nextui-org/react";
 
 export default function App() {
   return (
-    <Checkbox isDisabled={true} defaultSelected={true}>
+    <Checkbox isDisabled defaultSelected>
       Default
     </Checkbox>
   );
