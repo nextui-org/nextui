e9d056817b053700e1b914c36556f6fa55b8a747
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2022-11-06 05:32:37 +0800

    chore: custom theme for storybook docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,29 +1,78 @@
-import React from 'react';
-import { themes } from '@storybook/theming';
-import { useDarkMode } from 'storybook-dark-mode';
-import { createTheme, NextUIProvider, styled } from '@nextui-org/react';
+import React from "react";
+import {themes} from "@storybook/theming";
+import {useDarkMode} from "storybook-dark-mode";
+import {DocsContainer} from "@storybook/addon-docs";
+import {createTheme, NextUIProvider, styled} from "@nextui-org/react";
 
 const lightTheme = createTheme({
-  type: 'light',
-  className: 'light-theme'
+  type: "light",
+  className: "light-theme",
 });
 
 const darkTheme = createTheme({
-  type: 'dark',
-  className: 'dark-theme'
+  type: "dark",
+  className: "dark-theme",
 });
 
-const Box = styled('div', {
-  display: 'flex',
-  bg: '$background',
-  color: '$text',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexWrap: 'wrap',
-  width: '100vw',
-  height: '100vh'
+const Box = styled("div", {
+  size: "100%",
+  color: "$text",
+  bg: "$background",
+  variants: {
+    mode: {
+      docs: {
+        ".sbdocs": {
+          bg: "$accents0",
+        },
+        ".sbdocs-title, .sbdocs-h2, .sbdocs-h3": {
+          color: "$text",
+        },
+        ".docblock-emptyblock": {
+          border: "none",
+          color: "$text",
+          bg: "$accents1",
+          borderRadius: "$xs",
+        },
+        ".os-host-foreign": {
+          bg: "$accents1",
+        },
+        ".sbdocs-preview": {
+          boxShadow: "$sm",
+          borderRadius: "$xs",
+        },
+        "div.docs-story": {
+          bg: "none !important",
+          "div:has(button.docblock-code-toggle)": {
+            us: "none",
+            bg: "$accents1",
+            overflow: "hidden",
+            borderTopLeftRadius: "$xs",
+          },
+        },
+      },
+      canvas: {
+        display: "flex",
+        flexFlow: "row wrap",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: "$md $lg",
+      },
+    },
+  },
+  defaultVariants: {
+    mode: "canvas",
+  },
 });
+
+const DocsContainerTheme = (props) => {
+  return (
+    <NextUIProvider theme={useDarkMode() ? darkTheme : lightTheme}>
+      <Box mode="docs">
+        <DocsContainer {...props} />
+      </Box>
+    </NextUIProvider>
+  );
+};
 
 export const decorators = [
   (Story) => (
@@ -32,44 +81,46 @@ export const decorators = [
         <Story />
       </Box>
     </NextUIProvider>
-  )
+  ),
 ];
 
 export const parameters = {
-  layout: 'fullscreen',
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: "fullscreen",
+  actions: {argTypesRegex: "^on[A-Z].*"},
   darkMode: {
     stylePreview: true,
-    darkClass: 'dark-theme',
-    lightClass: 'light-theme',
-    dark: {
-      ...themes.dark,
-      // accent0, accent1
-      appBg: '#161616',
-      barBg: '#262626',
-      background: '#161616',
-      appContentBg: '#161616',
-      // radii xs
-      appBorderRadius: 7
-    },
+    darkClass: darkTheme.className,
+    lightClass: lightTheme.className,
     light: {
       ...themes.normal,
-      // accent0, accent1
-      appBg: '#F5F5F5',
-      barBg: '#EDEDED',
-      background: '#F5F5F5',
-      appContentBg: '#F5F5F5',
-      // radii xs
-      appBorderRadius: 7
-    }
+      appBg: "#F1F3F5",
+      barBg: "#ECEEF0",
+      background: "#F1F3F5",
+      appContentBg: "#F1F3F5",
+      appBorderRadius: 7,
+    },
+    dark: {
+      ...themes.dark,
+      appBg: "#16181A",
+      barBg: "#26292B",
+      background: "#16181A",
+      appContentBg: "#16181A",
+      appBorderRadius: 7,
+    },
   },
   backgrounds: {
-    default: 'light'
+    disable: true,
+    grid: {
+      disable: true,
+    },
   },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/
-    }
-  }
+      date: /Date$/,
+    },
+  },
+  docs: {
+    container: (props) => <DocsContainerTheme {...props} />,
+  },
 };
