9a52096ecd128f9a97e6d19cf5584c2a1c71afd6
ivan <ivan.xu@applovin.com>
2022-11-22 13:52:05 +0800

    feat(Link): external icon demo change custom icon

--- apps/docs/content/components/link/external.ts
+++ apps/docs/content/components/link/external.ts
@@ -1,4 +1,24 @@
-const App = `import { Link, Spacer, Badge, Loading } from "@nextui-org/react";
+const App = `import { Link, StyledLinkIcon, Spacer, Badge } from "@nextui-org/react";
+
+const CustomLink = () => {
+  return (
+    <StyledLinkIcon
+      className="custom-link-icon" 
+      width="1em" 
+      height="1em"
+      fill="none" 
+      viewBox="0 0 24 24"
+      shapeRendering="geometricPrecision"
+      stroke="currentColor" 
+      stroke-width="2" 
+      stroke-linecap="round" 
+      stroke-linejoin="round" 
+    >
+      <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+      <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+    </StyledLinkIcon>
+  )
+}
 
 export default function App() {
   return (
@@ -11,7 +31,7 @@ export default function App() {
         "First solve the problem. Then, write the code." - Jon Johnson.
       </Link>
       <Spacer />
-      <Link href="#" isExternal externalIcon={<Loading size="xs" color="success"/>} color="success">
+      <Link href="#" isExternal externalIcon={<CustomLink />} color="success">
         "First solve the problem. Then, write the code." - Jon Johnson.
       </Link>
     </>
