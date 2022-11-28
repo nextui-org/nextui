7ef7fdfdc37b0fb497bce019ccc739bac9de251c
Jakob Guddas <jakob.guddas@openmailbox.org>
2022-11-28 22:13:44 +0100

    fix: fixed issues of clear button inside inputs #920

--- packages/react/src/input/input.styles.ts
+++ packages/react/src/input/input.styles.ts
@@ -1,5 +1,6 @@
 import {styled, VariantProps} from "../theme/stitches.config";
 import {sharedVisuallyHidden} from "../theme/shared-css";
+import {cssFocusVisible} from "../theme/shared-css";
 
 const baseInputValues = {
   $$inputColor: "$colors$accents0",
@@ -884,6 +885,11 @@ export const StyledInputClearButton = styled("button", {
     },
   },
 });
+
+export const StyledInputClearButtonIconWrapper = styled('span', {
+  display: 'flex',
+  br: '100%'
+} , cssFocusVisible)
 
 // types
 export type InputVariantsProps = VariantProps<typeof StyledInput>;
