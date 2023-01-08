4f83b26c0b8716f1ff80947e422e96b8222faef2
Junior Garcia <jrgarciadev@gmail.com>
2023-01-07 22:00:05 -0300

    feat(root): initial storybook + tailwind config

--- packages/components/storybook/tailwind.config.js
+++ packages/components/storybook/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['../**/src/**/*.{js,jsx,ts,tsx}'],
   // Toggle dark-mode based on data-mode="dark"
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
