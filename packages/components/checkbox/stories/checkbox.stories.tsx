97ab097171ee633e3268cc27cae7bfadcc2396d6
Junior Garcia <jrgarciadev@gmail.com>
2023-03-05 00:30:14 -0300

    chore(checkbox): stories comments delete

--- packages/components/checkbox/stories/checkbox.stories.tsx
+++ packages/components/checkbox/stories/checkbox.stories.tsx
@@ -90,83 +90,16 @@ DisableAnimation.args = {
 export const Controlled = () => {
   const [selected, setSelected] = React.useState<boolean>(true);
 
-  // const [groupSelected, setGroupSelected] = React.useState<string[]>(["buenos-aires", "sydney"]);
-
   React.useEffect(() => {
     // eslint-disable-next-line no-console
     console.log("Checkbox ", selected);
   }, [selected]);
-
-  // React.useEffect(() => {
-  //   // eslint-disable-next-line no-console
-  //   console.log("CheckboxGroup ", groupSelected);
-  // }, [groupSelected]);
 
   return (
     <div className="flex flex-row gap-2">
       <Checkbox isSelected={selected} onChange={setSelected} {...checkbox.defaultVariants}>
         Subscribe (controlled)
       </Checkbox>
-      {/* <Checkbox.Group
-        color="warning"
-        label="Select cities"
-        labelColor="primary"
-        value={groupSelected}
-        onChange={setGroupSelected}
-      >
-        <Checkbox color="primary" value="buenos-aires">
-          Buenos Aires
-        </Checkbox>
-        <Checkbox labelColor="warning" value="sydney">
-          Sydney
-        </Checkbox>
-        <Checkbox labelColor="error" value="london">
-          London
-        </Checkbox>
-        <Checkbox value="tokyo">Tokyo</Checkbox>
-      </Checkbox.Group> */}
     </div>
   );
 };
-
-// export const Group = () => {
-//   // eslint-disable-next-line no-console
-//   const handleGroupChange = (value: string[]) => console.log(value);
-
-//   return (
-//     <Checkbox.Group
-//       color="warning"
-//       defaultValue={["buenos-aires"]}
-//       label="Select cities"
-//       labelColor="primary"
-//       onChange={handleGroupChange}
-//     >
-//       <Checkbox color="primary" value="buenos-aires">
-//         Buenos Aires
-//       </Checkbox>
-//       <Checkbox labelColor="warning" value="sydney">
-//         Sydney
-//       </Checkbox>
-//       <Checkbox isDisabled labelColor="error" value="london">
-//         London
-//       </Checkbox>
-//       <Checkbox value="tokyo">Tokyo</Checkbox>
-//     </Checkbox.Group>
-//   );
-// };
-
-// export const GroupRow = () => (
-//   <Checkbox.Group
-//     color="warning"
-//     defaultValue={["1"]}
-//     label="Select cities"
-//     orientation="horizontal"
-//   >
-//     <Checkbox color="primary" value="1">
-//       Buenos Aires
-//     </Checkbox>
-//     <Checkbox value="2">Sydney</Checkbox>
-//     <Checkbox value="3">London</Checkbox>
-//     <Checkbox value="4">Tokyo</Checkbox>
-//   </Checkbox.Group>
-// );
