6171d192f801fd82b6b03f57571c366d6f900fef
Armisael2k <chuylopez755@gmail.com>
2023-03-06 03:11:48 -0600

    new Skeleton delay api

--- apps/docs/content/components/image/showSkeleton.ts
+++ apps/docs/content/components/image/showSkeleton.ts
@@ -7,7 +7,7 @@ export default function App() {
       width={320}
       height={180}  
       maxDelay={10000}
-      src="http://www.deelay.me/10000/https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+      src="https://app.requestly.io/delay/10000/https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
       alt="Default Image"
     />
   );
