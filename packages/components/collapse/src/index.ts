7e1b49bad8fa83500f59a0a0988a8430d237e84e
Junior Garcia <jrgarciadev@gmail.com>
2023-03-19 11:37:19 -0300

    chore(root): collapse component deleted

--- packages/components/collapse/src/index.ts
+++
@@ -1,7 +0,0 @@
-// export types
-export type {CollapseProps} from "./collapse";
-export type {CollapseItemProps} from "./collapse-item";
-export type {RenderIndicatorProps} from "./base/collapse-item-base";
-
-// export component
-export {default as Collapse} from "./collapse";
