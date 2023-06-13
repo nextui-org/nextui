01086dbecfe43666d85a38e1a63b5f4aaa462be2
camcam <67684008+camcam2605@users.noreply.github.com>
2023-06-13 22:53:09 +0700

    Update classes.ts

--- packages/core/theme/src/utils/classes.ts
+++ packages/core/theme/src/utils/classes.ts
@@ -2,10 +2,10 @@
  * This is the base classNames for all elements.
  * Is meant to be used with the `addBase` method from tailwindcss.
  */
-export const baseStyles = (prefix) => {
+export const baseStyles = (prefix) => ({
   color: `hsl(var(--${prefix}-foreground))`,
   backgroundColor: `hsl(var(--${prefix}-background))`,
-};
+});
 
 /**
  * focus classNames when the element is focused by keyboard.
