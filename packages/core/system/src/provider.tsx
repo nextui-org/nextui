ced7e6093af6c791f62c2aa66f90f5ca4441ad18
Jakob Guddas <github@jguddas.de>
2023-06-21 01:08:49 +0200

    feat: added locale prop to NextUIProvider (#1145)
    * feat: added locale prop to NextUIProvider
    
    * fix: added missing locale prop in storybook config
    
    * chore: resolved PR comments

--- packages/core/system/src/provider.tsx
+++ packages/core/system/src/provider.tsx
@@ -1,14 +1,18 @@
 import {SSRProvider} from "@react-aria/ssr";
+import {I18nProvider, I18nProviderProps} from "@react-aria/i18n";
 import {OverlayProvider} from "@react-aria/overlays";
 
 export interface NextUIProviderProps {
   children: React.ReactNode;
+  locale?: I18nProviderProps["locale"];
 }
 
-export const NextUIProvider: React.FC<NextUIProviderProps> = ({children}) => {
+export const NextUIProvider: React.FC<NextUIProviderProps> = ({children, locale = "en"}) => {
   return (
     <SSRProvider>
-      <OverlayProvider>{children}</OverlayProvider>
+      <I18nProvider locale={locale}>
+        <OverlayProvider>{children}</OverlayProvider>
+      </I18nProvider>
     </SSRProvider>
   );
 };
