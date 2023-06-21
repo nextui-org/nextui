18b3f056ef70222769767f86df2937c6aea1a27b
Jakob Guddas <github@jguddas.de>
2023-06-21 18:47:29 +0200

    feat: added locale select to storybook toolbar

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -4,14 +4,19 @@ import {NextUIProvider} from "@nextui-org/react";
 import Style from "./style";
 
 export const decorators = [
-  (Story) => (
-    <NextUIProvider>
-      <div className="bg-dark">
-        <Style />
-        <Story />
-      </div>
-    </NextUIProvider>
-  ),
+  (Story, {globals: {locale}}) => {
+    const direction =
+      locale && new Intl.Locale(locale)?.textInfo?.direction === "rtl" ? "rtl" : undefined;
+
+    return (
+      <NextUIProvider locale={locale}>
+        <div className="bg-dark" lang={locale} dir={direction}>
+          <Style />
+          <Story />
+        </div>
+      </NextUIProvider>
+    );
+  },
 ];
 
 export const parameters = {
@@ -45,6 +50,56 @@ export const parameters = {
     light: {
       ...themes.light,
       appBorderRadius: 14,
+    },
+  },
+};
+
+const locales = [
+  "ar-AE",
+  "bg-BG",
+  "cs-CZ",
+  "da-DK",
+  "de-DE",
+  "el-GR",
+  "en-US",
+  "es-ES",
+  "et-EE",
+  "fi-FI",
+  "fr-FR",
+  "he-IL",
+  "hr-HR",
+  "hu-HU",
+  "it-IT",
+  "ja-JP",
+  "ko-KR",
+  "lt-LT",
+  "lv-LV",
+  "nb-NO",
+  "nl-NL",
+  "pl-PL",
+  "pt-BR",
+  "pt-PT",
+  "ro-RO",
+  "ru-RU",
+  "sk-SK",
+  "sl-SI",
+  "sr-SP",
+  "sv-SE",
+  "tr-TR",
+  "uk-UA",
+  "zh-CN",
+  "zh-TW",
+];
+
+export const globalTypes = {
+  locale: {
+    toolbar: {
+      icon: "globe",
+      items: locales.map((locale) => ({
+        value: locale,
+        title: new Intl.DisplayNames(undefined, {type: "language"}).of(locale),
+        right: new Intl.Locale(locale)?.textInfo?.direction === "rtl" ? "Right to Left" : undefined,
+      })),
     },
   },
 };
