01e764813dbd667eb2e1217565145b517b532ee9
Jakob Guddas <github@jguddas.de>
2023-07-15 15:48:21 +0200

    fix: added aria hide outside to popover (#1198)
    * fix: added aria hide outside and prevent scroll to popover
    
    * chore: readded react-remove-scroll

--- packages/components/popover/src/use-popover.ts
+++ packages/components/popover/src/use-popover.ts
@@ -4,12 +4,12 @@ import type {RefObject, Ref} from "react";
 
 import {OverlayTriggerState, useOverlayTriggerState} from "@react-stately/overlays";
 import {useFocusRing} from "@react-aria/focus";
-import {useOverlayTrigger} from "@react-aria/overlays";
+import {useOverlayTrigger, ariaHideOutside} from "@react-aria/overlays";
 import {OverlayTriggerProps} from "@react-types/overlays";
 import {HTMLNextUIProps, mapPropsVariants, PropGetter} from "@nextui-org/system";
 import {getArrowPlacement, getShouldUseAxisPlacement} from "@nextui-org/aria-utils";
 import {popover} from "@nextui-org/theme";
-import {mergeProps, mergeRefs} from "@react-aria/utils";
+import {mergeProps, mergeRefs, useLayoutEffect} from "@react-aria/utils";
 import {createDOMRef} from "@nextui-org/react-utils";
 import {clsx, dataAttr} from "@nextui-org/shared-utils";
 import {ReactRef} from "@nextui-org/react-utils";
@@ -222,6 +222,12 @@ export function usePopover(originalProps: UsePopoverProps) {
     }),
     [arrowProps, ariaPlacement, placementProp, slots, classNames],
   );
+
+  useLayoutEffect(() => {
+    if (state.isOpen && popoverRef.current) {
+      return ariaHideOutside([popoverRef.current]);
+    }
+  }, [state.isOpen, popoverRef]);
 
   return {
     Component,
