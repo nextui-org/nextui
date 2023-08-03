c7a1376872c7802c525ef012270d057d0e221ef2
Junior Garcia <jrgarciadev@gmail.com>
2023-08-02 23:50:38 -0300

    fix(docs): poster added to blog videos, play/pause button added

--- apps/docs/components/blog/video-in-view.tsx
+++ apps/docs/components/blog/video-in-view.tsx
@@ -5,11 +5,14 @@ import {useInView} from "framer-motion";
 import {useRef, FC, useEffect, useState, useCallback} from "react";
 import {Button, cn, Spinner, Tooltip} from "@nextui-org/react";
 
+import {PlayBoldIcon, PauseBoldIcon} from "@/components/icons";
 import {RotateLeftLinearIcon} from "@/components/icons";
 
 interface VideoInViewProps {
   src: string;
+  playMode?: "auto" | "manual";
   autoPlay?: boolean;
+  poster?: string;
   width?: number;
   height?: number;
   controls?: boolean;
@@ -20,18 +23,25 @@ export const VideoInView: FC<VideoInViewProps> = ({
   src,
   width,
   height,
+  poster,
   autoPlay = true,
+  playMode = "manual",
   controls = false,
   className,
 }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const isVisible = useInView(videoRef);
 
-  // play video when it is visible
+  // play video when it is visible and playMode is auto
   useEffect(() => {
+    if (playMode !== "auto") {
+      return;
+    }
+
     if (isVisible) {
       videoRef.current?.play();
     } else {
@@ -39,19 +49,53 @@ export const VideoInView: FC<VideoInViewProps> = ({
     }
   }, [isVisible]);
 
-  const handleCanPlayThrough = useCallback(() => {
+  const handleCanPlay = useCallback(() => {
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const videoEl = videoRef.current;
+
+    if (videoEl) {
+      if (videoEl.readyState > 3) {
+        // HAVE_FUTURE_DATA: enough data to start playing
+        handleCanPlay();
+      } else {
+        videoEl.addEventListener("canplaythrough", handleCanPlay);
+      }
+
+      // Cleanup the event listener
+      return () => {
+        videoEl.removeEventListener("canplaythrough", handleCanPlay);
+      };
+    }
   }, []);
 
   const onRestart = useCallback(() => {
     if (videoRef.current) {
       videoRef.current.currentTime = 0;
       videoRef.current.play();
+      setIsPlaying(true);
     }
   }, []);
 
+  const onTogglePlay = useCallback(() => {
+    if (videoRef.current) {
+      if (!isPlaying) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+
+      setIsPlaying((v) => !v);
+    }
+  }, [isPlaying]);
+
   return (
-    <div className="relative">
+    <div
+      className="relative data-[playing=true]:after:opacity-0 data-[playing=true]:after:z-[-1] after:content-[''] after:absolute after:inset-0 after:bg-black/30 after:z-20 after:transition-opacity"
+      data-playing={isPlaying}
+    >
       {isLoading && (
         <Spinner
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
@@ -59,6 +103,22 @@ export const VideoInView: FC<VideoInViewProps> = ({
           size="lg"
         />
       )}
+      <Tooltip content={isPlaying ? "Pause" : "Play"} delay={1000}>
+        <Button
+          isIconOnly
+          className="absolute z-50 right-12 top-3 border-1 border-transparent bg-transparent before:bg-white/10 before:content-[''] before:block before:z-[-1] before:absolute before:inset-0 before:backdrop-blur-md before:backdrop-saturate-100 before:rounded-lg"
+          size="sm"
+          variant="bordered"
+          onPress={onTogglePlay}
+        >
+          {isPlaying ? (
+            <PauseBoldIcon className="text-white" size={16} />
+          ) : (
+            <PlayBoldIcon className="text-white" size={16} />
+          )}
+        </Button>
+      </Tooltip>
+
       <Tooltip content="Restart" delay={1000}>
         <Button
           isIconOnly
@@ -67,24 +127,24 @@ export const VideoInView: FC<VideoInViewProps> = ({
           variant="bordered"
           onPress={onRestart}
         >
-          <RotateLeftLinearIcon size={16} />
+          <RotateLeftLinearIcon className="text-white" size={16} />
         </Button>
       </Tooltip>
       <video
         ref={videoRef}
         loop
         muted
-        autoPlay={autoPlay}
+        autoPlay={autoPlay && playMode === "auto"}
         className={cn(
           "w-full border border-transparent dark:border-default-200/50 object-fit rounded-xl shadow-lg",
           className,
         )}
         controls={controls}
         height={height}
+        poster={poster}
         src={src}
         width={width}
-        onCanPlayThrough={handleCanPlayThrough}
-        onPlaying={() => setIsLoading(false)}
+        onCanPlay={handleCanPlay}
       />
     </div>
   );
---
+++ apps/docs/components/icons/bold/play.tsx
@@ -0,0 +1,19 @@
+import {IconSvgProps} from "@/types";
+
+export const PlayBoldIcon = ({size = 24, width, height, ...props}: IconSvgProps) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height={size || height}
+    role="presentation"
+    viewBox="0 0 24 24"
+    width={size || width}
+    {...props}
+  >
+    <path
+      d="M4 11.9999V8.43989C4 4.01989 7.13 2.2099 10.96 4.4199L14.05 6.1999L17.14 7.9799C20.97 10.1899 20.97 13.8099 17.14 16.0199L14.05 17.7999L10.96 19.5799C7.13 21.7899 4 19.9799 4 15.5599V11.9999Z"
+      fill="currentColor"
+    />
+  </svg>
+);
