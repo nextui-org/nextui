92a59b59c9c61ecce1c11b3d81e8a402a281576d
Junior Garcia <jrgarciadev@gmail.com>
2023-08-04 15:13:27 -0300

    fix(docs): radio & checkbox custom styles

--- apps/docs/content/components/radio-group/custom-styles.ts
+++ apps/docs/content/components/radio-group/custom-styles.ts
@@ -8,7 +8,7 @@ export const CustomRadio = (props) => {
       {...otherProps}
       classNames={{
         base: cn(
-          "inline-flex bg-content1 hover:bg-content2 items-center justify-between",
+          "inline-flex m-0 bg-content1 hover:bg-content2 items-center justify-between",
           "flex-row-reverse max-w-[300px] cursor-pointer rounded-lg gap-4 p-4 border-2 border-transparent",
           "data-[selected=true]:border-primary"
         ),
