b7c947805d770156a95496cc03b544908a61ed72
Junior Garcia <jrgarciadev@gmail.com>
2023-08-08 00:12:21 -0300

    feat(storybook): logo adapted to light/dark theme, favicon added

--- packages/storybook/.storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import {create} from "@storybook/theming";
-
-export default create({
-  brandTitle: "NextUI",
-  brandUrl: "https://nextui.org",
-  brandImage: "https://nextui.org/storybook-logo.png",
-  brandTarget: "_self",
-});
