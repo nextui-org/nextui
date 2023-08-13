01c08efe461fd52808166c2ad4f2c86466af9acc
Junior Garcia <jrgarciadev@gmail.com>
2023-08-12 23:45:10 -0300

    fix(docs): navbar with search input example

--- apps/docs/content/components/navbar/with-search-input.ts
+++ apps/docs/content/components/navbar/with-search-input.ts
@@ -79,12 +79,13 @@ export default function App() {
         <Input
           classNames={{
             base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
             input: "text-small",
-            inputWrapper: "font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
           }}
           placeholder="Type to search..."
           size="sm"
-          startContent={<SearchLinearIcon size={18} />}
+          startContent={<SearchIcon size={18} />}
           type="search"
         />
         <Dropdown placement="bottom-end">
