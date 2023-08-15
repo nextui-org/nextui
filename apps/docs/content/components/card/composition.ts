f2909a00e17ce91c6b139c878af2b0757334bda4
Cesar Leonardo Insuasty Moreno <40036889+cinsuasty@users.noreply.github.com>
2023-08-15 13:21:37 -0500

    fix: Import CardFooter in the card component doc

--- apps/docs/content/components/card/composition.ts
+++ apps/docs/content/components/card/composition.ts
@@ -1,4 +1,4 @@
-const App = `import {Card, CardHeader, CardBody, Avatar, Button} from "@nextui-org/react";
+const App = `import {Card, CardHeader, CardBody, CardFooter, Avatar, Button} from "@nextui-org/react";
 
 export default function App() {
   const [isFollowed, setIsFollowed] = React.useState(false);
