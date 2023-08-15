cfa76ab594a9b2bf7160cbefe30b8f656a5bae04
remorses <beats.by.morse@gmail.com>
2023-08-15 18:12:38 +0200

    fix(input): don't pass value to useTextField

--- packages/components/input/src/use-input.ts
+++ packages/components/input/src/use-input.ts
@@ -125,7 +125,6 @@ export function useInput<T extends HTMLInputElement | HTMLTextAreaElement = HTML
         originalProps?.placeholder,
       ),
       inputElementType: isMultiline ? "textarea" : "input",
-      value: inputValue, // don't pass "" if value === undefined, it will switch the input to controlled mode
       onChange: setInputValue,
     },
     domRef,
