021aa16d966068621eb9933b4fd36dece61f92ca
Junior Garcia <jrgarciadev@gmail.com>
2023-08-26 11:30:06 -0300

    chore(docs): inter font applied to tailwind config

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -61,7 +61,7 @@ module.exports = {
         highlighted: `${commonColors.purple[500]} 1px 0 0, ${commonColors.purple[500]} -1px 0 0`,
       },
       fontFamily: {
-        sans: ["DM Sans, sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
         serif: defaultTheme.fontFamily.serif,
         mono: defaultTheme.fontFamily.mono,
       },
