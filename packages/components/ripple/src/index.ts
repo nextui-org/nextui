92fd22a6042ba9af7c42f6f12f408be97ccbf9c9
Chongyi Zheng <git@zcy.dev>
2023-09-10 01:42:18 -0400

    fix: export `RippleType`

--- packages/components/ripple/src/index.ts
+++ packages/components/ripple/src/index.ts
@@ -2,6 +2,7 @@ import Ripple from "./ripple";
 
 // export types
 export type {RippleProps} from "./ripple";
+export type {RippleType} from "./use-ripple";
 
 // export hooks
 export {useRipple} from "./use-ripple";
