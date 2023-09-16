a3daf978df868365ad355d41d35b7919d68f9145
lijianan <574980606@qq.com>
2023-09-16 23:51:22 +0800

    Update packages/components/card/src/use-card.ts
    Co-authored-by: Jakob Guddas <github@jguddas.de>

--- packages/components/card/src/use-card.ts
+++ packages/components/card/src/use-card.ts
@@ -85,7 +85,7 @@ export function useCard(originalProps: UseCardProps) {
 
   const baseStyles = clsx(classNames?.base, className);
 
-  const {onClick: onRippleClickHandler, onClear, ripples} = useRipple();
+  const {onClick: onRippleClickHandler, onClear: onClearRipple, ripples} = useRipple();
 
   const handleClick = (e: MouseEvent<HTMLDivElement>) => {
     if (!originalProps.disableAnimation && !disableRipple && domRef.current) {
