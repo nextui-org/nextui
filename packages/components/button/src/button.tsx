28a20180b523291830f90fe6288a707d08487398
栗嘉男 <574980606@qq.com>
2023-09-22 17:53:47 +0800

    button: add letter-spacing style for button children

--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -3,8 +3,15 @@ import {Ripple} from "@nextui-org/ripple";
 import {forwardRef} from "@nextui-org/system";
 
 import {UseButtonProps, useButton} from "./use-button";
+import React from "react";
 
-export interface ButtonProps extends UseButtonProps {}
+export interface ButtonProps extends UseButtonProps { }
+
+const twoCNChar = /^[\u4e00-\u9fa5]{2}$/;
+
+function isTwoCNChar(child: React.ReactNode) {
+  return React.Children.count(child) === 1 && typeof child === "string" && twoCNChar.test(child);
+}
 
 const Button = forwardRef<"button", ButtonProps>((props, ref) => {
   const {
@@ -21,10 +28,15 @@ const Button = forwardRef<"button", ButtonProps>((props, ref) => {
     disableRipple,
     getButtonProps,
     getRippleProps,
-  } = useButton({...props, ref});
+  } = useButton({ ...props, ref });
+  
+  const mergedClassNames = React.useMemo<string>(
+    () => isTwoCNChar(children) ? [styles, 'tracking-[0.5em]'].join(" ") : styles,
+    [children, styles],
+  );
 
   return (
-    <Component ref={domRef} className={styles} {...getButtonProps()}>
+    <Component ref={domRef} className={mergedClassNames} {...getButtonProps()}>
       {startContent}
       {isLoading && spinnerPlacement === "start" && spinner}
       {children}
