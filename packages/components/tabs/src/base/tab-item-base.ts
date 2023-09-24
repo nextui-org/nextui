283ea84558e0b21da188ce8c37649b2be0879225
Junior Garcia <jrgarciadev@gmail.com>
2023-09-24 12:07:07 -0300

    fix(tabs): typecheck

--- packages/components/tabs/src/base/tab-item-base.ts
+++ packages/components/tabs/src/base/tab-item-base.ts
@@ -1,5 +1,4 @@
 import {BaseItem, ItemProps} from "@nextui-org/aria-utils";
-import {AriaTabProps} from "@react-types/tabs";
 import {ReactNode} from "react";
 interface Props<T extends object = {}> extends Omit<ItemProps<"button", T>, "children" | "title"> {
   /**
@@ -15,9 +14,13 @@ interface Props<T extends object = {}> extends Omit<ItemProps<"button", T>, "chi
    *  This will be used as native `title` attribute.
    * */
   titleValue?: string;
+  /** Whether the tab should be disabled. */
+  isDisabled?: boolean;
+  /** Whether the tab selection should occur on press up instead of press down. */
+  shouldSelectOnPressUp?: boolean;
 }
 
-export type TabItemProps<T extends object = {}> = Props<T> & AriaTabProps;
+export type TabItemProps<T extends object = {}> = Props<T>;
 
 const TabItemBase = BaseItem as <T extends object>(props: TabItemProps<T>) => JSX.Element;
 
