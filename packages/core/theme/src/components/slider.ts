9c0987b5816bb240597b85d8bf49ee600f4a08c4
Junior Garcia <jrgarciadev@gmail.com>
2023-09-24 16:41:15 -0300

    fix(slider): animation

--- packages/core/theme/src/components/slider.ts
+++ packages/core/theme/src/components/slider.ts
@@ -116,7 +116,7 @@ const slider = tv({
         thumb: "data-[dragging=true]:after:scale-100",
       },
       false: {
-        thumb: "motion-reduce:after:transition-all",
+        thumb: "motion-reduce:after:transition-all motion-reduce:after:transition-none",
       },
     },
   },
