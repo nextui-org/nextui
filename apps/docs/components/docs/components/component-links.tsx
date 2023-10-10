3ed77e097feb0d2b0fabd5baf1cee67c0645da91
jqkk <kkj2551@naver.com>
2023-10-10 10:49:27 +0900

    docs: fix storybook link in ComponentLinks

--- apps/docs/components/docs/components/component-links.tsx
+++ apps/docs/components/docs/components/component-links.tsx
@@ -72,7 +72,9 @@ export const ComponentLinks = ({
   return (
     <div className="flex flex-wrap gap-3 mt-6">
       <ButtonLink
-        href={`https://storybook.nextui.org/?path=/story/components-${storybook || component}`}
+        href={`https://storybook.nextui.org/?path=/story/components-${
+          storybook || component
+        }--default`}
         startContent={<StorybookIcon className="text-lg text-[#ff4785]" />}
       >
         Storybook
