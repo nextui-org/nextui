2bc432f44a09ddf2861402838aece0806668c783
Junior Garcia <jrgarciadev@gmail.com>
2023-10-26 11:48:51 -0300

    feat(scroll-shadow): logic improved, controlled support added

--- packages/components/scroll-shadow/src/index.ts
+++ packages/components/scroll-shadow/src/index.ts
@@ -2,6 +2,7 @@ import ScrollShadow from "./scroll-shadow";
 
 // export types
 export type {ScrollShadowProps} from "./scroll-shadow";
+export type {VisibleState} from "@nextui-org/use-data-scroll-overflow";
 
 // export hooks
 export {useScrollShadow} from "./use-scroll-shadow";
--- packages/components/scroll-shadow/stories/scroll-shadow.stories.tsx
+++ packages/components/scroll-shadow/stories/scroll-shadow.stories.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {Meta} from "@storybook/react";
-import {scrollShadow} from "@nextui-org/theme";
+import {scrollShadow, button} from "@nextui-org/theme";
 import Lorem from "react-lorem-component";
 
-import {ScrollShadow, ScrollShadowProps} from "../src";
+import {ScrollShadow, ScrollShadowProps, VisibleState} from "../src";
 
 export default {
   title: "Components/ScrollShadow",
@@ -15,6 +15,10 @@ export default {
     },
     offset: {
       control: {type: "number"},
+    },
+    visible: {
+      control: {type: "select"},
+      options: ["auto", "top", "bottom", "both", "left", "right"],
     },
     children: {
       table: {
@@ -33,14 +37,84 @@ export default {
 
 const defaultProps = {
   ...scrollShadow.defaultVariants,
+  visible: "auto",
   className: "w-[300px] h-[400px]",
   children: <Lorem count={10} />,
 };
 
 const Template = (args: ScrollShadowProps) => <ScrollShadow {...args} />;
 
+const ControlledTemplate = ({children, ...args}: ScrollShadowProps) => {
+  type Orientation = "vertical" | "horizontal";
+
+  const [visible, setVisible] = React.useState<VisibleState>("top");
+  const [orientation, setOrientation] = React.useState<Orientation>("vertical");
+
+  const states: Record<Orientation, VisibleState[]> = {
+    ["vertical"]: ["top", "bottom", "both"],
+    ["horizontal"]: ["left", "right", "both"],
+  };
+
+  const orientationStates: Orientation[] = ["vertical", "horizontal"];
+
+  return (
+    <div className="flex flex-col gap-3">
+      <ScrollShadow
+        {...args}
+        className={orientation === "horizontal" ? "max-w-[300px] max-h-[400px]" : args.className}
+        orientation={orientation}
+        visible={visible}
+      >
+        {orientation === "horizontal" ? <div className="w-[800px]">{children}</div> : children}
+      </ScrollShadow>
+      <p className="text-default-500">Orientation: {orientation}</p>
+      <p className="text-default-500">Visible state: {visible}</p>
+      <div className="flex mt-2 gap-2">
+        {orientationStates.map((o) => (
+          <button
+            key={o}
+            className={button({
+              color: orientation === o ? "primary" : "default",
+            })}
+            onClick={() => {
+              if (o === "horizontal") {
+                setVisible("left");
+              } else {
+                setVisible("top");
+              }
+              setOrientation(o);
+            }}
+          >
+            {o}
+          </button>
+        ))}
+      </div>
+      <div className="flex mt-2 gap-2">
+        {states[orientation].map((state) => (
+          <button
+            key={state}
+            className={button({
+              color: visible === state ? "primary" : "default",
+            })}
+            onClick={() => setVisible(state)}
+          >
+            {state}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export const Default = {
   render: Template,
+  args: {
+    ...defaultProps,
+  },
+};
+
+export const Controlled = {
+  render: ControlledTemplate,
   args: {
     ...defaultProps,
   },
