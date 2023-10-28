41606e7ec0fc48264b08769d35af9fa28ef9ad7c
Junior Garcia <jrgarciadev@gmail.com>
2023-10-28 12:19:45 -0300

    chore(core): tailwind-variants upgraded, custom tv function adapted

--- packages/core/theme/src/utils/tv.ts
+++ packages/core/theme/src/utils/tv.ts
@@ -1,36 +1,31 @@
-import {tv as tvBase, TV} from "tailwind-variants";
+import {createTV} from "tailwind-variants";
 
 import {mappedSpacingScaleKeys} from "../types";
 const COMMON_UNITS = ["small", "medium", "large"];
 
-export const tv: TV = (options, config) =>
-  tvBase(options, {
-    ...config,
-    twMerge: config?.twMerge ?? true,
-    twMergeConfig: {
-      ...config?.twMergeConfig,
-      theme: {
-        ...config?.twMergeConfig?.theme,
-        opacity: ["disabled"],
-        spacing: ["divider", "unit", ...mappedSpacingScaleKeys],
-        borderWidth: COMMON_UNITS,
-        borderRadius: COMMON_UNITS,
-      },
-      classGroups: {
-        ...config?.twMergeConfig?.classGroups,
-        shadow: [{shadow: COMMON_UNITS}],
-        "font-size": [{text: ["tiny", ...COMMON_UNITS]}],
-        "bg-image": ["bg-stripe-gradient"],
-        "min-w": [
-          {
-            "min-w": ["unit", ...mappedSpacingScaleKeys],
-          },
-        ],
-        "min-h": [
-          {
-            "min-h": ["unit", ...mappedSpacingScaleKeys],
-          },
-        ],
-      },
+export const tv = createTV({
+  twMerge: true,
+  twMergeConfig: {
+    theme: {
+      opacity: ["disabled"],
+      spacing: ["divider", "unit", ...mappedSpacingScaleKeys],
+      borderWidth: COMMON_UNITS,
+      borderRadius: COMMON_UNITS,
     },
-  });
+    classGroups: {
+      shadow: [{shadow: COMMON_UNITS}],
+      "font-size": [{text: ["tiny", ...COMMON_UNITS]}],
+      "bg-image": ["bg-stripe-gradient"],
+      "min-w": [
+        {
+          "min-w": ["unit", ...mappedSpacingScaleKeys],
+        },
+      ],
+      "min-h": [
+        {
+          "min-h": ["unit", ...mappedSpacingScaleKeys],
+        },
+      ],
+    },
+  },
+});
