f9f9a567b0ab8d8cd58cf3edf73c9269c0a99ad7
Junior Garcia <jrgarciadev@gmail.com>
2023-11-08 10:15:48 -0300

    fix(textarea): styles issues and start/end content props added

---
+++ packages/utilities/shared-icons/src/bold/plus.tsx
@@ -0,0 +1,21 @@
+import {IconSvgProps} from "../types";
+
+export const PlusFilledIcon = (props: IconSvgProps) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="1em"
+    role="presentation"
+    viewBox="0 0 24 24"
+    width="1em"
+    {...props}
+  >
+    <path
+      clipRule="evenodd"
+      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM12.75 9C12.75 8.58579 12.4142 8.25 12 8.25C11.5858 8.25 11.25 8.58579 11.25 9L11.25 11.25H9C8.58579 11.25 8.25 11.5858 8.25 12C8.25 12.4142 8.58579 12.75 9 12.75H11.25V15C11.25 15.4142 11.5858 15.75 12 15.75C12.4142 15.75 12.75 15.4142 12.75 15L12.75 12.75H15C15.4142 12.75 15.75 12.4142 15.75 12C15.75 11.5858 15.4142 11.25 15 11.25H12.75V9Z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
+  </svg>
+);
