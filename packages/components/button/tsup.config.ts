bcfb74777ca54e9676fa031fcaa1f60a9d757e6a
remorses <beats.by.morse@gmail.com>
2023-11-14 18:18:40 +0100

    chore: remove tsup, use tsc

--- packages/components/button/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineConfig} from "tsup";
-
-export default defineConfig({
-  clean: true,
-  target: "es2019",
-  format: ["cjs", "esm"],
-  banner: {js: '"use client";'},
-});
--- packages/core/react/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineConfig} from "tsup";
-
-export default defineConfig({
-  clean: true,
-  target: "es2019",
-  entry: ["src/index.ts", "!src/scripts"],
-  format: ["cjs", "esm"],
-});
