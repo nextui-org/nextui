854e698ad5db67ef9c21179a2d58d2ad102d68c6
Ken <116851212+khaymanii@users.noreply.github.com>
2024-01-04 09:38:17 +0100

    Update mouse-enter.ts with the required logic
    I added the logic for the feature as requested in issue #2190. which says;
    
    "Trigger dropdown menu on mouse enter event"

--- apps/docs/content/components/dropdown/mouse-enter.ts
+++ apps/docs/content/components/dropdown/mouse-enter.ts
@@ -1,1 +1,40 @@
+const App = `import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
 
+export default function App() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+     <Dropdown
+      isOpen={isOpen}
+     >
+      <DropdownTrigger>
+        <Button 
+          variant="bordered" 
+           onMouseEnter={() => {
+              setIsOpen(true);
+            }}
+            onMouseLeave={() => {
+              setIsOpen(false);
+            }}
+        >
+          Open Menu
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem key="new">New file</DropdownItem>
+        <DropdownItem key="copy">Copy link</DropdownItem>
+        <DropdownItem key="edit">Edit file</DropdownItem>
+        <DropdownItem key="delete" className="text-danger" color="danger">
+          Delete file
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}`;
+
+const react = {
+  "/App.jsx": App,
+};
+
+export default {
+  ...react,
+};
