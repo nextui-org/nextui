41fe028ea1398c50dda48654169c640a5d427e64
haruki <soccer_haruki15@me.com>
2024-03-04 06:23:38 +0900

    docs(components): accordion controlled example shows errors

--- apps/docs/content/components/accordion/controlled.ts
+++ apps/docs/content/components/accordion/controlled.ts
@@ -1,7 +1,7 @@
 const App = `import {Accordion, AccordionItem} from "@nextui-org/react";
 
 export default function App() {
-  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set(["1"]));
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["1"]));
 
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
