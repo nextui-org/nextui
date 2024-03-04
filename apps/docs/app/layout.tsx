56137f6d7aa11d42a40ce7d50fff7fd0953668ad
Tsuki <976499226@qq.com>
2024-03-04 22:12:10 +0800

    refactor(docs): update layout.tsx

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/sandpack.css";
 import {Metadata} from "next";
-import Script from "next/script";
 import {clsx} from "@nextui-org/shared-utils";
 
 import {Providers} from "./providers";
@@ -78,19 +77,6 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
           </div>
           <Cmdk />
         </Providers>
-        <Script
-          defer
-          data-modal-disclaimer="This is a custom LLM for NextUI with access to all developer docs (nextui.org/docs) and GitHub Issues and PRs (github.com/nextui-org/nextui)."
-          data-modal-example-questions="How do I install for Next.js?,How do I customize primary color?"
-          data-project-color="#000000"
-          data-project-logo="https://avatars.githubusercontent.com/u/86160567?s=280&v=4"
-          data-project-name="NextUI"
-          data-website-id="e733a73f-980e-4f7d-9e8b-91867453f899"
-          src="https://widget.kapa.ai/kapa-widget.bundle.js"
-          strategy="afterInteractive"
-        />
-        {__PROD__ && <Analytics />}
-        <Script id="featurebase-sdk" src="https://do.featurebase.app/js/sdk.js" />
         <ScriptProviders />
       </body>
     </html>
