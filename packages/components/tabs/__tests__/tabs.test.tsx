c6a4d03835ce826c37dfd39e2c30b89f5c477172
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-04-02 17:10:52 +0800

    feat(tabs): add test for lazy motion issue

--- packages/components/tabs/__tests__/tabs.test.tsx
+++ packages/components/tabs/__tests__/tabs.test.tsx
@@ -31,6 +31,10 @@ let tabs: Item[] = [
 
 describe("Tabs", () => {
   it("should render correctly (static)", () => {
+    // e.g. console.error Warning: Function components cannot be given refs.
+    // Attempts to access this ref will fail. Did you mean to use React.forwardRef()?
+    const spy = jest.spyOn(console, "error").mockImplementation(() => {});
+
     const wrapper = render(
       <Tabs aria-label="Tabs static test">
         <Tab key="item1" title="Item 1">
@@ -46,6 +50,10 @@ describe("Tabs", () => {
     );
 
     expect(() => wrapper.unmount()).not.toThrow();
+
+    expect(spy).toBeCalledTimes(0);
+
+    spy.mockRestore();
   });
 
   it("should render correctly (dynamic)", () => {
