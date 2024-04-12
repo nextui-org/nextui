d6b812d680ab3c6c1c0c392b363a901d825d6407
HaRuki Kuriwada <haruki.kuriwada@hennge.com>
2024-04-12 16:36:00 +0900

    fix: fixed some example components styles

--- apps/docs/content/components/date-input/label-placements.ts
+++ apps/docs/content/components/date-input/label-placements.ts
@@ -10,7 +10,6 @@ export default function App() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-2">
-        <h3 className="text-default-500 text-small">Without placeholder</h3>
         <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
           {placements.map((placement) => (
             <DateInput 
--- apps/docs/content/components/date-input/min-and-max-date.ts
+++ apps/docs/content/components/date-input/min-and-max-date.ts
@@ -4,16 +4,22 @@ import {getLocalTimeZone, parseDate, today} from "@internationalized/date";
 export default function App() {
   return (
     <div className="w-full max-w-xl flex flex-row gap-4">
-      <DateInput
-        label="Date and time"
-        minValue={today(getLocalTimeZone())}
-        defaultValue={parseDate("2024-04-03")}
-      />
-      <DateInput
-        label="Date and time"
-        maxValue={today(getLocalTimeZone())}
-        defaultValue={parseDate("2024-04-05")}
-      />
+      <div className="flex flex-col gap-2">
+        <h3>Min date</h3>
+        <DateInput
+          label="Date and time"
+          minValue={today(getLocalTimeZone())}
+          defaultValue={parseDate("2024-04-03")}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h3>Max date</h3>
+        <DateInput
+          label="Date and time"
+          maxValue={today(getLocalTimeZone())}
+          defaultValue={parseDate("2024-04-05")}
+        />
+      </div>
     </div>
   );
 }`;
