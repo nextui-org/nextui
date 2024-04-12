ec485ad803d7ae1cf74d37197a80a3afeed5b236
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-04-12 16:52:36 +0800

    chore(docs): remove padding and revise gap

--- apps/docs/content/components/calendar/usage.ts
+++ apps/docs/content/components/calendar/usage.ts
@@ -3,7 +3,7 @@ import {parseDate} from '@internationalized/date';
 
 export default function App() {
   return (
-    <div className="flex gap-4 p-4">
+    <div className="flex gap-x-4">
       <Calendar aria-label="Date (No Selection)" />
       <Calendar aria-label="Date (Uncontrolled)" defaultValue={parseDate("2020-02-03")} />
     </div>
