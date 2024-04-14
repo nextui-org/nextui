90870b30484ef4b2db5ce65375620add79337bac
haruki <soccer_haruki15@me.com>
2024-04-14 12:44:50 +0900

    fix: added variants section to the doc

--- apps/docs/content/components/date-picker/index.ts
+++ apps/docs/content/components/date-picker/index.ts
@@ -2,6 +2,7 @@ import usage from "./usage";
 import disabled from "./disabled";
 import readOnly from "./readonly";
 import required from "./required";
+import variants from "./variants";
 import labelPlacements from "./label-placements";
 import description from "./description";
 import errorMessage from "./error-message";
@@ -23,6 +24,7 @@ export const datePickerContent = {
   disabled,
   readOnly,
   required,
+  variants,
   labelPlacements,
   description,
   errorMessage,
