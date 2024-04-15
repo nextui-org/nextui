a3398ffc235fc335d6bee07f5c21454799cc4b24
Junior Garcia <jrgarciadev@gmail.com>
2024-04-15 13:33:47 -0300

    chore(docs): blog changes

--- apps/docs/content/components/date-input/usage.ts
+++ apps/docs/content/components/date-input/usage.ts
@@ -4,7 +4,7 @@ import {CalendarDate} from "@internationalized/date";
 export default function App() {
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <DateInput label={"Birth date"} placeholderValue={new CalendarDate(1995, 11, 6)} />
+      <DateInput label={"Birth date"} placeholderValue={new CalendarDate(1995, 11, 6)} className="max-w-sm" />
     </div>
   );
 }`;
