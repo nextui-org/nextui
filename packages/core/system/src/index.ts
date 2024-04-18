002d82e3707f5628d2c08dd47af4907ad95ec548
Junior Garcia <jrgarciadev@gmail.com>
2024-04-18 14:13:21 -0300

    fix(core): export * from not supported on client components

--- packages/core/system/src/index.ts
+++ packages/core/system/src/index.ts
@@ -1,6 +1,35 @@
-export * from "./provider";
-export * from "./provider-context";
+export type {
+  As,
+  DOMElement,
+  DOMElements,
+  CapitalizedDOMElements,
+  DOMAttributes,
+  OmitCommonProps,
+  RightJoinProps,
+  MergeWithAs,
+  InternalForwardRefRenderFunction,
+  PropsOf,
+  Merge,
+  HTMLNextUIProps,
+  PropGetter,
+  ExtendVariantProps,
+  ExtendVariantWithSlotsProps,
+  ExtendVariants,
+} from "@nextui-org/system-rsc";
 
-export * from "@nextui-org/system-rsc";
+export {
+  cn,
+  forwardRef,
+  toIterator,
+  mapPropsVariants,
+  mapPropsVariantsWithCommon,
+  isNextUIEl,
+  extendVariants,
+} from "@nextui-org/system-rsc";
 
 export type {SupportedCalendars} from "./types";
+export type {NextUIProviderProps} from "./provider";
+export type {ProviderContextProps} from "./provider-context";
+
+export {NextUIProvider} from "./provider";
+export {ProviderContext, useProviderContext} from "./provider-context";
--- packages/utilities/react-utils/src/index.ts
+++ packages/utilities/react-utils/src/index.ts
@@ -1,6 +1,28 @@
-export * from "./context";
-export * from "./refs";
-export * from "./dom";
-export * from "./dimensions";
+export type {CreateContextOptions, CreateContextReturn} from "./context";
+export {createContext} from "./context";
+
+export type {ReactRef} from "./refs";
+export {assignRef, mergeRefs} from "./refs";
+
+export type {UserAgentBrowser, UserAgentOS, ContextValue, UserAgentDeviceType} from "./dom";
+export {
+  isBrowser,
+  canUseDOM,
+  getUserAgentBrowser,
+  getUserAgentOS,
+  detectOS,
+  detectDeviceType,
+  detectBrowser,
+  detectTouch,
+  createDOMRef,
+  createFocusableRef,
+  useDOMRef,
+  useFocusableRef,
+  useSyncRef,
+  areRectsIntersecting,
+} from "./dom";
+
+export type {ShapeType} from "./dimensions";
+export {getCSSStyleVal, getRealShape} from "./dimensions";
 
 export * from "@nextui-org/react-rsc-utils";
