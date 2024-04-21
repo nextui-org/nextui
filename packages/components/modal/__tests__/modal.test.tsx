a64ec22a79bb2d3d32d26f26491e6aaddbd41b53
wzc520pyfm <1528857653@qq.com>
2024-04-21 17:47:04 +0800

    test(components): [modal] add draggable modal tests

--- packages/components/modal/__tests__/modal.test.tsx
+++ packages/components/modal/__tests__/modal.test.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 import {act, render, fireEvent} from "@testing-library/react";
 
-import {Modal, ModalContent, ModalBody, ModalHeader, ModalFooter} from "../src";
+import {Modal, ModalContent, ModalBody, ModalHeader, ModalFooter, useDraggable} from "../src";
 
 // e.g. console.error Warning: Function components cannot be given refs.
 // Attempts to access this ref will fail. Did you mean to use React.forwardRef()?
 const spy = jest.spyOn(console, "error").mockImplementation(() => {});
+
+const ModalDraggable = ({overflow = false}) => {
+  const targetRef = React.useRef(null);
+  const dragRef = React.useRef(null);
+
+  useDraggable({targetRef, dragRef, overflow});
+
+  return (
+    <Modal ref={targetRef} isOpen>
+      <ModalContent>
+        <ModalHeader ref={dragRef}>Modal header</ModalHeader>
+        <ModalBody>Modal body</ModalBody>
+        <ModalFooter>Modal footer</ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 describe("Modal", () => {
   afterEach(() => {
@@ -107,5 +124,59 @@ describe("Modal", () => {
 
     fireEvent.keyDown(modal, {key: "Escape"});
     expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("should be rendered a draggable modal", () => {
+    // mock viewport size to 1920x1080
+    jest.spyOn(document.documentElement, "clientWidth", "get").mockImplementation(() => 1920);
+    jest.spyOn(document.documentElement, "clientHeight", "get").mockImplementation(() => 1080);
+    const wrapper = render(<ModalDraggable />);
+
+    const modal = wrapper.getByRole("dialog");
+    const modalHeader = wrapper.getByText("Modal header");
+
+    fireEvent.mouseDown(modalHeader, {clientX: 0, clientY: 0});
+    fireEvent.mouseMove(modalHeader, {clientX: 100, clientY: 50});
+    fireEvent.mouseUp(modalHeader, {clientX: 100, clientY: 50});
+
+    expect(() => wrapper.unmount()).not.toThrow();
+    expect(document.documentElement.clientWidth).toBe(1920);
+    expect(document.documentElement.clientHeight).toBe(1080);
+    expect(modalHeader.style.cursor).toBe("move");
+    expect(modal.style.transform).toBe("translate(100px, 50px)");
+  });
+
+  it("should not drag overflow viewport", () => {
+    // mock viewport size to 1920x1080
+    jest.spyOn(document.documentElement, "clientWidth", "get").mockImplementation(() => 1920);
+    jest.spyOn(document.documentElement, "clientHeight", "get").mockImplementation(() => 1080);
+    const wrapper = render(<ModalDraggable />);
+    const modal = wrapper.getByRole("dialog");
+    const modalHeader = wrapper.getByText("Modal header");
+
+    fireEvent.mouseDown(modalHeader, {clientX: 100, clientY: 50});
+    fireEvent.mouseMove(modalHeader, {clientX: 10000, clientY: 5000});
+    fireEvent.mouseUp(modalHeader, {clientX: 10000, clientY: 5000});
+
+    expect(modal.style.transform).toBe("translate(1920px, 1080px)");
+  });
+
+  test("should be rendered a draggable modal with overflow", () => {
+    // mock viewport size to 1920x1080
+    jest.spyOn(document.documentElement, "clientWidth", "get").mockImplementation(() => 1920);
+    jest.spyOn(document.documentElement, "clientHeight", "get").mockImplementation(() => 1080);
+
+    const wrapper = render(<ModalDraggable overflow />);
+
+    const modal = wrapper.getByRole("dialog");
+    const modalHeader = wrapper.getByText("Modal header");
+
+    fireEvent.mouseDown(modalHeader, {clientX: 0, clientY: 0});
+    fireEvent.mouseMove(modalHeader, {clientX: 2000, clientY: 1500});
+    fireEvent.mouseUp(modalHeader, {clientX: 2000, clientY: 1500});
+
+    expect(document.documentElement.clientWidth).toBe(1920);
+    expect(document.documentElement.clientHeight).toBe(1080);
+    expect(modal.style.transform).toBe("translate(2000px, 1500px)");
   });
 });
