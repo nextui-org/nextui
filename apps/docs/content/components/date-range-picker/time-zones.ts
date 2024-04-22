1cc5215ce6026da7e7736d74a5479b2f5b86c1ff
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-04-22 22:38:22 +0900

    fix: missing aria labels in date range picker (#2832)
    * fix(date-range-picker): ensure label props are reflected in aria labels
    
    * docs: added missing label to date range picker
    
    * chore: add changesets
    
    * Update .changeset/gentle-pigs-admire.md
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/docs/content/components/date-range-picker/time-zones.ts
+++ apps/docs/content/components/date-range-picker/time-zones.ts
@@ -1,4 +1,4 @@
-const AppTs = `import {DateRangePicker} from "@nextui-org/react";
+const App = `import {DateRangePicker} from "@nextui-org/react";
 import {parseZonedDateTime, parseAbsoluteToLocal} from "@internationalized/date";
 
 export default function App() {
@@ -9,6 +9,7 @@ export default function App() {
           start: parseZonedDateTime("2024-04-01T00:45[America/Los_Angeles]"),
           end: parseZonedDateTime("2024-04-14T11:15[America/Los_Angeles]"),
         }}
+        label="Stay duration"
         labelPlacement="outside"
       />
       <DateRangePicker
@@ -16,14 +17,15 @@ export default function App() {
           start: parseAbsoluteToLocal("2024-04-01T07:45:00Z"),
           end: parseAbsoluteToLocal("2024-04-14T19:15:00Z"),
         }}
+        label="Stay duration"
         labelPlacement="outside"
       />
-  </div>
+    </div>
   );
 }`;
 
 const react = {
-  "/App.tsx": AppTs,
+  "/App.jsx": App,
 };
 
 export default {
