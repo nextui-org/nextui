d4b3679254c2a15c5480ab78fa93e316ec1792f7
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-05-04 19:50:20 +0800

    fix(system): missing useHref logic

---
+++ .changeset/curvy-eels-tap.md
@@ -0,0 +1,5 @@
+---
+"@nextui-org/system": patch
+---
+
+Fix missing useHref logic (#2934)
--- packages/core/system/src/provider.tsx
+++ packages/core/system/src/provider.tsx
@@ -1,5 +1,6 @@
 import type {ModalProviderProps} from "@react-aria/overlays";
 import type {ProviderContextProps} from "./provider-context";
+import type {Href} from "@react-types/shared";
 
 import {I18nProvider, I18nProviderProps} from "@react-aria/i18n";
 import {RouterProvider} from "@react-aria/utils";
@@ -23,11 +24,20 @@ export interface NextUIProviderProps
    * Link, Menu, Tabs, Table, etc.
    */
   navigate?: (path: string) => void;
+  /**
+   * Convert an `href` provided to a link component to a native `href`
+   * For example, a router might accept hrefs relative to a base path,
+   * or offer additional custom ways of specifying link destinations.
+   * The original href specified on the link is passed to the navigate function of the RouterProvider,
+   * and useHref is used to generate the full native href to put on the actual DOM element.
+   */
+  useHref?: (href: Href) => string;
 }
 
 export const NextUIProvider: React.FC<NextUIProviderProps> = ({
   children,
   navigate,
+  useHref,
   locale = "en-US",
   defaultDates = {
     minDate: new CalendarDate(1900, 1, 1),
@@ -39,7 +49,11 @@ export const NextUIProvider: React.FC<NextUIProviderProps> = ({
   let contents = children;
 
   if (navigate) {
-    contents = <RouterProvider navigate={navigate}>{contents}</RouterProvider>;
+    contents = (
+      <RouterProvider navigate={navigate} useHref={useHref}>
+        {contents}
+      </RouterProvider>
+    );
   }
 
   const context = useMemo<ProviderContextProps>(
