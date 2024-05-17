096b0516aa269aae84bfd8f3078e2a30f123b620
WK Wong <wingkwong.code@gmail.com>
2024-05-17 18:16:03 +0800

    fix(slider): calculate the correct value on mark click

--- packages/components/slider/src/use-slider.ts
+++ packages/components/slider/src/use-slider.ts
@@ -389,6 +389,30 @@ export function useSlider(originalProps: UseSliderProps) {
       style: {
         [isVertical ? "bottom" : direction === "rtl" ? "right" : "left"]: `${percent * 100}%`,
       },
+      // avoid `onDownTrack` is being called since when you click the mark,
+      // `onDownTrack` will calculate the percent based on the position you click
+      // the calculated value will be set instead of the actual value defined in `marks`
+      onMouseDown: (e: React.MouseEvent) => e.stopPropagation(),
+      onPointerDown: (e: React.PointerEvent) => e.stopPropagation(),
+      onClick: (e: any) => {
+        e.stopPropagation();
+        if (state.values.length === 1) {
+          state.setThumbPercent(0, percent);
+        } else {
+          const leftThumbPos = state.values[0];
+          const rightThumbPos = state.values[1];
+
+          if (mark.value < leftThumbPos) {
+            state.setThumbPercent(0, percent);
+          } else if (mark.value > rightThumbPos) {
+            state.setThumbPercent(1, percent);
+          } else if (Math.abs(mark.value - leftThumbPos) < Math.abs(mark.value - rightThumbPos)) {
+            state.setThumbPercent(0, percent);
+          } else {
+            state.setThumbPercent(1, percent);
+          }
+        }
+      },
     };
   };
 
