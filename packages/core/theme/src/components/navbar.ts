a48bd6a79de4740f3e8ca166c32b473e57a55f04
nozomemein <b8yukifsukeo999n@gmail.com>
2024-05-26 08:36:24 +0900

    fix(navbar): fixed unnecessary changes

--- packages/core/theme/src/components/navbar.ts
+++ packages/core/theme/src/components/navbar.ts
@@ -165,7 +165,6 @@ const navbar = tv({
       "flex",
       "max-w-full",
       "top-[var(--navbar-height)]",
-      "h-[calc(100vh_-_var(--navbar-height))]",
       "inset-x-0",
       "bottom-0",
       "w-screen",
@@ -231,7 +230,7 @@ const navbar = tv({
     },
     disableAnimation: {
       true: {
-        menu: ["hidden", "data-[open=true]:flex"],
+        menu: ["hidden", "h-[calc(100vh_-_var(--navbar-height))]", "data-[open=true]:flex"],
       },
     },
   },
