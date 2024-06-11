68779022e8b5d37a9e751d8c847b08cb8cf2cb37
WK Wong <wingkwong.code@gmail.com>
2024-06-11 18:00:09 +0800

    fix(utilities): include shouldFocus logic

--- packages/utilities/aria-utils/src/overlays/ariaShouldCloseOnInteractOutside.ts
+++ packages/utilities/aria-utils/src/overlays/ariaShouldCloseOnInteractOutside.ts
@@ -1,4 +1,4 @@
-import {RefObject} from "react";
+import {MutableRefObject, RefObject} from "react";
 
 /**
  * Used to handle the outside interaction for popover-based components
@@ -6,16 +6,22 @@ import {RefObject} from "react";
  * @param element - the element outside of the popover ref, originally from `shouldCloseOnInteractOutside`
  * @param triggerRef - The trigger ref object
  * @param state - The state from the popover component
+ * @param shouldFocus - a mutable ref boolean object to control the focus state
+ *                      (used in input-based component such as autocomplete)
  * @returns - a boolean value which is same as shouldCloseOnInteractOutside
  */
 export const ariaShouldCloseOnInteractOutside = (
   element: Element,
   triggerRef: RefObject<Element>,
   state: any,
+  shouldFocus?: MutableRefObject<boolean>,
 ) => {
   const trigger = triggerRef?.current!;
 
   if (!trigger || !trigger.contains(element)) {
+    // blur the component (e.g. autocomplete)
+    if (shouldFocus) shouldFocus.current = false;
+
     const startElements = document.querySelectorAll("body > span[data-focus-scope-start]");
     let focusScopeElements: Element[] = [];
 
@@ -28,6 +34,9 @@ export const ariaShouldCloseOnInteractOutside = (
 
       return false;
     }
+  } else {
+    // otherwise the component (e.g. autocomplete) should keep focused
+    if (shouldFocus) shouldFocus.current = true;
   }
 
   return !trigger || !trigger.contains(element);
