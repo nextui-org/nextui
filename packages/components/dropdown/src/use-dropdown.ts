8fd22daef9eee05fd0813be0bb89a47658f7b082
WK Wong <wingkwong.code@gmail.com>
2024-06-11 16:27:50 +0800

    fix(dropdown): add ariaShouldCloseOnInteractOutside

--- packages/components/dropdown/src/use-dropdown.ts
+++ packages/components/dropdown/src/use-dropdown.ts
@@ -8,6 +8,7 @@ import {useMenuTrigger} from "@react-aria/menu";
 import {dropdown} from "@nextui-org/theme";
 import {clsx} from "@nextui-org/shared-utils";
 import {ReactRef, mergeRefs} from "@nextui-org/react-utils";
+import {ariaShouldCloseOnInteractOutside} from "@nextui-org/aria-utils";
 import {useMemo, useRef} from "react";
 import {mergeProps} from "@react-aria/utils";
 import {MenuProps} from "@nextui-org/menu";
@@ -123,7 +124,7 @@ export function useDropdown(props: UseDropdownProps) {
       },
       shouldCloseOnInteractOutside: popoverProps?.shouldCloseOnInteractOutside
         ? popoverProps.shouldCloseOnInteractOutside
-        : () => true,
+        : (element: Element) => ariaShouldCloseOnInteractOutside(element, triggerRef, state),
     };
   };
 
