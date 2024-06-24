5efa8fe305ffde773888909809c754cdec62b11f
WK Wong <wingkwong.code@gmail.com>
2024-06-24 21:36:45 +0800

    feat(shared-utils): add clamp

--- packages/utilities/shared-utils/src/functions.ts
+++ packages/utilities/shared-utils/src/functions.ts
@@ -85,3 +85,15 @@ export function isEmpty(obj: Extractable) {
     [Object, Array].includes(((obj || {}) as any).constructor) && !Object.entries(obj || {}).length
   );
 }
+
+export function clamp(number: number, boundOne: number, boundTwo: number) {
+  if (!boundTwo) {
+    return Math.max(number, boundOne) === boundOne ? number : boundOne;
+  } else if (Math.min(number, boundOne) === number) {
+    return boundOne;
+  } else if (Math.max(number, boundTwo) === number) {
+    return boundTwo;
+  }
+
+  return number;
+}
