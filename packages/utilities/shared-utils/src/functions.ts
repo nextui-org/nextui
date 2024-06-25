6934dd33da18d754d7743b08297ddfcc4fd891e0
WK Wong <wingkwong.code@gmail.com>
2024-06-25 12:51:15 +0800

    refactor(shared-utils): remove isEmpty and clamp

--- packages/utilities/shared-utils/src/functions.ts
+++ packages/utilities/shared-utils/src/functions.ts
@@ -80,24 +80,6 @@ export function objectToDeps(obj: Extractable) {
   }
 }
 
-export function isEmpty(obj: Extractable) {
-  return (
-    [Object, Array].includes(((obj || {}) as any).constructor) && !Object.entries(obj || {}).length
-  );
-}
-
-export function clamp(number: number, boundOne: number, boundTwo: number) {
-  if (!boundTwo) {
-    return Math.max(number, boundOne) === boundOne ? number : boundOne;
-  } else if (Math.min(number, boundOne) === number) {
-    return boundOne;
-  } else if (Math.max(number, boundTwo) === number) {
-    return boundTwo;
-  }
-
-  return number;
-}
-
 export function debounce<F extends (...args: any[]) => void>(
   func: F,
   waitMilliseconds: number = 0,
