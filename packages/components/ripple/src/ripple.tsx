b5c948a2e2a2d65fd0f49192b83f3424e600731b
WK Wong <wingkwong.code@gmail.com>
2024-06-29 13:40:01 +0800

    refactor(ripple): load domAnimation dynamically

--- packages/components/ripple/src/ripple.tsx
+++ packages/components/ripple/src/ripple.tsx
@@ -3,7 +3,7 @@ import type {FC} from "react";
 import type {HTMLMotionProps} from "framer-motion";
 import type {HTMLNextUIProps} from "@nextui-org/system";
 
-import {AnimatePresence, m, LazyMotion, domAnimation} from "framer-motion";
+import {AnimatePresence, m, LazyMotion} from "framer-motion";
 import {clamp} from "@nextui-org/shared-utils";
 
 export interface RippleProps extends HTMLNextUIProps<"span"> {
@@ -16,6 +16,8 @@ export interface RippleProps extends HTMLNextUIProps<"span"> {
 
 const Ripple: FC<RippleProps> = (props) => {
   const {ripples = [], motionProps, color = "currentColor", style, onClear} = props;
+
+  const domAnimation = () => import("./domAnimation").then((res) => res.default);
 
   return (
     <>
