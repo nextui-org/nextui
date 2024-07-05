91f4c5aee4f379d0ae3209465443223deaa26441
WK Wong <wingkwong.code@gmail.com>
2024-07-05 22:59:59 +0800

    feat(autocomplete): useHiddenInput

--- packages/components/autocomplete/src/hidden-input.tsx
+++ packages/components/autocomplete/src/hidden-input.tsx
@@ -1,8 +1,12 @@
 import type {ComboBoxState} from "@react-stately/combobox";
 
-import React, {ReactNode} from "react";
+import React, {ReactNode, RefObject} from "react";
+import {useFormReset} from "@react-aria/utils";
+import {useFormValidation} from "@react-aria/form";
 
-export interface AriaHiddenInputProps<T> {
+import {inputData} from "./use-autocomplete";
+
+export interface AriaHiddenInputProps {
   /**
    * Describes the type of autocomplete functionality the input should provide if any.
    * See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#htmlattrdefautocomplete).
@@ -16,20 +20,63 @@ export interface AriaHiddenInputProps<T> {
   isDisabled?: boolean;
   /** Whether the input is required. */
   isRequired?: boolean;
-  /** State for the input. */
-  state: ComboBoxState<T>;
 }
 
-export function HiddenInput<T>(props: AriaHiddenInputProps<T>) {
-  const {state, isDisabled, isRequired, ...otherProps} = props;
+type NativeHTMLInputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  keyof AriaHiddenInputProps
+>;
 
-  return (
-    <input
-      {...otherProps}
-      disabled={isDisabled}
-      required={isRequired}
-      type="hidden"
-      value={state.selectedKey || ""}
-    />
+type CombinedAriaInputProps = NativeHTMLInputProps & AriaHiddenInputProps;
+
+export interface HiddenInputProps<T> extends CombinedAriaInputProps {
+  /** State for the input. */
+  state: ComboBoxState<T>;
+  /** A ref to the hidden `<input>` element. */
+  inputRef?: RefObject<HTMLInputElement>;
+}
+
+export function useHiddenInput<T>(props: HiddenInputProps<T>) {
+  const data = inputData.get(props.state) || {};
+
+  const {
+    state,
+    autoComplete,
+    name = data.name,
+    isDisabled = data.isDisabled,
+    inputRef,
+    onChange,
+  } = props;
+
+  const {validationBehavior, isRequired} = data;
+
+  useFormReset(props.inputRef!, state.selectedKey, state.setSelectedKey);
+  useFormValidation(
+    {
+      validationBehavior,
+      focus: () => inputRef?.current?.focus(),
+    },
+    state,
+    inputRef,
   );
+
+  return {
+    name,
+    ref: inputRef,
+    type: "hidden",
+    disabled: isDisabled,
+    required: isRequired,
+    autoComplete,
+    value: state.selectedKey ?? "",
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+      state.setSelectedKey(e.target.value);
+      onChange?.(e);
+    },
+  };
+}
+
+export function HiddenInput<T>(props: HiddenInputProps<T>) {
+  const inputProps = useHiddenInput(props);
+
+  return <input {...inputProps} />;
 }
