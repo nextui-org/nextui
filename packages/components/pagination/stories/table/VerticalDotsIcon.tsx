a522d63dca842022bbdacc26c37902e0cdacdb33
arindam1997007 <arindamchy00@gmail.com>
2024-07-11 21:54:07 +0530

    chore: modified pagination.stories for testing

---
+++ packages/components/pagination/stories/table/VerticalDotsIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {IconSvgProps} from "./types";
+
+export const VerticalDotsIcon = ({size = 24, width, height, ...props}: IconSvgProps) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height={size || height}
+    role="presentation"
+    viewBox="0 0 24 24"
+    width={size || width}
+    {...props}
+  >
+    <path
+      d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+      fill="currentColor"
+    />
+  </svg>
+);
---
+++ packages/components/pagination/stories/table/types.ts
@@ -0,0 +1,5 @@
+import {SVGProps} from "react";
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
