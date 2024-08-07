a831ea36770b34e0b58f1116482102aa0a293b29
WK Wong <wingkwong.code@gmail.com>
2024-08-08 00:15:23 +0800

    chore(deps): bump framer-motion version

--- apps/docs/components/sandpack/use-sandpack.ts
+++ apps/docs/components/sandpack/use-sandpack.ts
@@ -70,7 +70,7 @@ export const useSandpack = ({
   }, {});
 
   let dependencies = {
-    "framer-motion": "11.3.8",
+    "framer-motion": "11.3.23",
     "@nextui-org/react": "latest",
   };
 
@@ -139,7 +139,7 @@ export const useSandpack = ({
 
   // const dependencies = useMemo(() => {
   //   let deps = {
-  //     "framer-motion": "11.3.8",
+  //     "framer-motion": "11.3.23",
   //   };
 
   //   if (hasComponents) {
