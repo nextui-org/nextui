547a9dc3430fec997f035d5bddbf6eaa08c34593
WK Wong <wingkwong.code@gmail.com>
2024-08-23 18:37:52 +0800

    feat(alert): init alert storybook structure

--- packages/components/alert/stories/alert.stories.tsx
+++ packages/components/alert/stories/alert.stories.tsx
@@ -1,6 +1,19 @@
 import {Meta} from "@storybook/react";
+import {alert} from "@nextui-org/theme";
+
+import {Alert} from "../src";
 
 export default {
   title: "Components/Alert",
-  Component: alert,
-} as Meta<typeof alert>;
+  Component: Alert,
+} as Meta<typeof Alert>;
+
+const defaultProps = {
+  ...alert.defaultVariants,
+};
+
+export const Default = {
+  args: {
+    ...defaultProps,
+  },
+};
