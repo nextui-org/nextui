4fcb4c02f11ab5cb94c775dc6e081bfe1be22b4f
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-08-31 15:25:44 +0530

    feat(alert): completed the styling

--- packages/core/theme/src/components/alert.ts
+++ packages/core/theme/src/components/alert.ts
@@ -21,8 +21,10 @@ import {tv} from "../utils/tv";
  */
 const alert = tv({
   slots: {
-    base: ["group flex flex-row justify-start items-start data-[hidden=true]:hidden px-4 py-3"],
-    title: ["text-medium  font-medium text-foreground-100 block"],
+    base: [
+      "group flex flex-row w-[350px] justify-start items-start data-[hidden=true]:hidden px-4 py-3",
+    ],
+    title: ["text-medium text-foreground-100 block"],
     description: ["text-small text-foreground-100 block"],
     mainWrapper: ["w-full px-3 flex flex-col h-full box-border items-start justify-center"],
     closeButton: [],
@@ -36,28 +38,28 @@ const alert = tv({
       },
       primary: {
         base: ["bg-primary-50", "border-primary-200"],
-        title: ["text-primary-200"],
-        description: ["text-primary-200"],
+        title: ["text-primary-500"],
+        description: ["text-primary-500"],
       },
       secondary: {
         base: ["bg-secondary-50", "border-secondary-200"],
-        title: ["text-secondary-200"],
-        description: ["text-secondary-200"],
+        title: ["text-secondary-500"],
+        description: ["text-secondary-500"],
       },
       success: {
         base: ["bg-success-50", "border-success-200"],
-        title: ["text-success-200"],
-        description: ["text-success-200"],
+        title: ["text-success-300"],
+        description: ["text-success-300"],
       },
       warning: {
         base: ["bg-warning-50", "border-warning-200"],
-        title: ["text-warning-200"],
-        description: ["text-warning-200"],
+        title: ["text-warning-300"],
+        description: ["text-warning-300"],
       },
       danger: {
         base: ["bg-danger-50", "border-danger-200"],
-        title: ["text-danger-200"],
-        description: ["text-danger-200"],
+        title: ["text-danger-300"],
+        description: ["text-danger-300"],
       },
     },
     radius: {
