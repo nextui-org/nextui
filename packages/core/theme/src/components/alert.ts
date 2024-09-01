81552c89d37a691adc893094c21ec0927382a404
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-09-01 21:35:03 +0530

    feat(alert): make styling more accurate

--- packages/core/theme/src/components/alert.ts
+++ packages/core/theme/src/components/alert.ts
@@ -2,64 +2,72 @@ import type {VariantProps} from "tailwind-variants";
 
 import {tv} from "../utils/tv";
 /**
- * Input wrapper **Tailwind Variants** component
+ * Alert wrapper **Tailwind Variants** component
  *
  * @example
  * ```js
  * const {base, mainWrapper, title, description} = input({...})
  *
  * <div className={base())}>
- *     // start content comes here if present
- *      <div classsName ={mainWrapper()}>
- *        <div className={title()}>Title</div>
- *        <div className={description()}>Description</div>
+ *    <div className = {icon()}>
+ *    // icon
  *     </div>
- *    // end content comes here if present
- *    // close button
+ *
+ *    <div className = "innerwrapper">
+ *        // start content comes here if present
+ *         <div classsName ={mainWrapper()}>
+ *           <div className={title()}>Title</div>
+ *           <div className={description()}>Description</div>
+ *        </div>
+ *       // end content comes here if present
+ *     </div>
+ *
+ *    <button>
+ *      // close button
+ *    </button>
  * </div>
  * ```
  */
+
 const alert = tv({
   slots: {
-    base: [
-      "group flex flex-row w-[350px] justify-start items-start data-[hidden=true]:hidden px-4 py-3",
-    ],
-    title: ["text-medium text-foreground-100 block"],
-    description: ["text-small text-foreground-100 block"],
-    mainWrapper: ["w-full px-3 flex flex-col h-full box-border items-start justify-center"],
-    closeButton: [],
+    base: ["group flex flex-row w-[342px] h-[68px]  p-[12px]"],
+    title: ["text-medium font-normal block h-[24px]"],
+    description: ["text-small font-normal h-[20px]"],
+    mainWrapper: ["w-[268px] h-[44px] ml-[20px] flex flex-col box-border items-start"],
+    closeButton: ["w-[24px h-[24px] cursor-pointer relative"],
   },
   variants: {
     color: {
       default: {
-        base: ["bg-default-200", "border-default-200"],
-        title: ["text-white"],
-        description: ["text-white"],
+        base: ["bg-default-100"],
+        title: ["text-foreground"],
+        description: ["text-[default-600]"],
       },
       primary: {
-        base: ["bg-primary-50", "border-primary-200"],
-        title: ["text-primary-500"],
-        description: ["text-primary-500"],
+        base: ["bg-primary-50"],
+        title: ["text-primary"],
+        description: ["text-primary"],
       },
       secondary: {
-        base: ["bg-secondary-50", "border-secondary-200"],
-        title: ["text-secondary-500"],
-        description: ["text-secondary-500"],
+        base: ["bg-secondary-50"],
+        title: ["text-secondary"],
+        description: ["text-secondary"],
       },
       success: {
-        base: ["bg-success-50", "border-success-200"],
-        title: ["text-success-300"],
-        description: ["text-success-300"],
+        base: ["bg-success-50"],
+        title: ["text-success"],
+        description: ["text-success"],
       },
       warning: {
-        base: ["bg-warning-50", "border-warning-200"],
-        title: ["text-warning-300"],
-        description: ["text-warning-300"],
+        base: ["bg-warning-50"],
+        title: ["text-[#F7B750]"],
+        description: ["text-[#F7B750]"],
       },
       danger: {
-        base: ["bg-danger-50", "border-danger-200"],
-        title: ["text-danger-300"],
-        description: ["text-danger-300"],
+        base: ["bg-danger-50 "],
+        title: ["text-[#F31260]"],
+        description: ["text-[#F31260]"],
       },
     },
     radius: {
@@ -67,21 +75,22 @@ const alert = tv({
         base: "rounded-none",
       },
       sm: {
-        base: "rounded-sm",
+        base: "rounded-[12px]",
       },
       md: {
-        base: "rounded-md",
+        base: "rounded-[16px]",
       },
       lg: {
-        base: "rounded-lg",
+        base: "rounded-[22px]",
       },
       full: {
-        base: "rounded-full",
+        base: "rounded-[9999px]",
       },
     },
   },
   defaultVariants: {
     color: "default",
+    radius: "md",
   },
 });
 
