3b77e2ed722dcec944405236ac5f07b7fae2962d
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-09-18 15:15:29 +0100

    Update accordion-tree.tsx
    Removing clsx.

--- packages/components/accordion/src/accordion-tree.tsx
+++ packages/components/accordion/src/accordion-tree.tsx
@@ -5,7 +5,6 @@ import {
   AccordionItemProps
 } from '@nextui-org/accordion';
 import { forwardRef, ReactNode } from 'react';
-import clsx from 'clsx';
 
 export interface AccordionTreeItemProps
   extends Omit<AccordionItemProps, 'children'> {
@@ -17,97 +16,53 @@ export interface AccordionTreeProps extends Omit<AccordionProps, 'children'> {
   items: AccordionTreeItemProps[];
 }
 
-const AccordionTree = forwardRef<'div', AccordionTreeProps>((props, ref) => {
-  const {
-    items,
-    itemClasses: propsItemClasses,
-    className: propsClassName,
-    isCompact = true,
-    selectionMode = 'multiple',
-    ...otherProps
-  } = props;
+const AccordionTree = forwardRef<HTMLDivElement, AccordionTreeProps>(
+  (props, ref) => {
+    const {
+      items,
+      itemClasses: propsItemClasses,
+      className,
+      isCompact = true,
+      selectionMode = 'multiple',
+      ...otherProps
+    } = props;
 
-  const itemClasses = {
-    ...propsItemClasses,
-    trigger: clsx('py-1', propsItemClasses?.trigger),
-    base: clsx('py-0', propsItemClasses?.base)
-  };
+    const itemClasses = {
+      ...propsItemClasses,
+      trigger: propsItemClasses?.trigger ?? 'py-1',
+      base: propsItemClasses?.base ?? 'py-0'
+    };
 
-  const className = clsx(propsClassName);
-
-  return (
-    <Accordion
-      {...otherProps}
-      selectionMode={selectionMode}
-      isCompact={isCompact}
-      itemClasses={itemClasses}
-      className={className}
-    >
-      {items.map((item, index) => {
-        const { contentMain, contentChildren, ...itemProps } = item;
-        return (
-          <AccordionItem key={index} {...itemProps}>
-            {contentMain && contentMain}
-            {contentChildren && (
-              <AccordionTree
-                itemClasses={itemClasses}
-                isCompact={isCompact}
-                selectionMode={selectionMode}
-                items={contentChildren}
-              />
-            )}
-          </AccordionItem>
-        );
-      })}
-    </Accordion>
-  );
-});
+    return (
+      <Accordion
+        ref={ref}
+        {...otherProps}
+        selectionMode={selectionMode}
+        isCompact={isCompact}
+        itemClasses={itemClasses}
+        className={className}
+      >
+        {items.map((item, index) => {
+          const { contentMain, contentChildren, ...itemProps } = item;
+          return (
+            <AccordionItem key={index} {...itemProps}>
+              {contentMain && contentMain}
+              {contentChildren && (
+                <AccordionTree
+                  itemClasses={itemClasses}
+                  isCompact={isCompact}
+                  selectionMode={selectionMode}
+                  items={contentChildren}
+                />
+              )}
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    );
+  }
+);
 
 AccordionTree.displayName = 'NextUI.AccordionTree';
 
 export default AccordionTree;
-
-//
-// export function AccordionTree({
-//   items,
-//   itemClasses: propsItemClasses,
-//   className: propsClassName,
-//   isCompact = true,
-//   selectionMode = 'multiple',
-//   ...props
-// }: { items: AccordionTreeItemProps[] } & Omit<AccordionProps, 'children'>) {
-//   const itemClasses = {
-//     ...propsItemClasses,
-//     trigger: clsx('py-1', propsItemClasses?.trigger),
-//     base: clsx('py-0', propsItemClasses?.base)
-//   };
-//
-//   const className = clsx(propsClassName);
-//
-//   return (
-//     <Accordion
-//       {...props}
-//       selectionMode={selectionMode}
-//       isCompact={isCompact}
-//       itemClasses={itemClasses}
-//       className={className}
-//     >
-//       {items.map((item, index) => {
-//         const { contentMain, contentChildren, ...itemProps } = item;
-//         return (
-//           <AccordionItem key={index} {...itemProps}>
-//             {contentMain && contentMain}
-//             {contentChildren && (
-//               <AccordionTree
-//                 itemClasses={itemClasses}
-//                 isCompact={isCompact}
-//                 selectionMode={selectionMode}
-//                 items={contentChildren}
-//               />
-//             )}
-//           </AccordionItem>
-//         );
-//       })}
-//     </Accordion>
-//   );
-// }
