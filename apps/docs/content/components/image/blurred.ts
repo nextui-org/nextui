974f08215a76ba4e1d4db96fcfe3c35147e1fce9
WK Wong <wingkwong.code@gmail.com>
2024-10-05 01:02:47 +0800

    fix: incorrect domain for images

--- apps/docs/content/components/image/blurred.ts
+++ apps/docs/content/components/image/blurred.ts
@@ -5,7 +5,7 @@ export default function App() {
     <Image
       isBlurred
       width={240}
-      src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
+      src="https://nextui.org/images/album-cover.png"
       alt="NextUI Album Cover"
       className="m-5"
     />
