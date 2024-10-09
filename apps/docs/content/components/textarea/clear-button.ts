41a29ffdeb4c5f30983ee59be727b99f76c5e5ea
doki- <1335902682@qq.com>
2024-10-09 23:47:20 +0800

    docs(textarea): add documentation to textarea

---
+++ apps/docs/content/components/textarea/clear-button.ts
@@ -0,0 +1,23 @@
+const App = `import {Textarea} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <Textarea
+      isClearable
+      label="Email"
+      variant="bordered"
+      placeholder="Enter your email"
+      defaultValue="junior@nextui.org"
+      onClear={() => console.log("input cleared")}
+      className="max-w-xs"
+    />
+  );
+}`;
+
+const react = {
+  "/App.jsx": App,
+};
+
+export default {
+  ...react,
+};
