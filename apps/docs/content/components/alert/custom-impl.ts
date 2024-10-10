7202be5eb08ff3825a3b3cabfb2d6e6f2fad9d14
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-10-10 20:28:22 +0530

    chore(alert): add props for closeIcon

--- apps/docs/content/components/alert/custom-impl.ts
+++ apps/docs/content/components/alert/custom-impl.ts
@@ -26,6 +26,7 @@ const CloseIcon = `export const CloseIcon = (props) => (
     strokeWidth={2}
     viewBox="0 0 24 24"
     width="1em"
+    {...props}
   >
     <path d="M18 6L6 18M6 6l12 12" />
   </svg>
