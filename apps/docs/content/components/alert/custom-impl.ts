61df0e407bf9a788af7864664877a06824c5f14f
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-10-10 20:16:26 +0530

    chore(alert): add import for closeIcon

--- apps/docs/content/components/alert/custom-impl.ts
+++ apps/docs/content/components/alert/custom-impl.ts
@@ -1,4 +1,4 @@
-const InfoCircleIcon = `export const InfoCircleIcon = (props) => {
+const InfoCircleIcon = `export const InfoCircleIcon = (props) => (
   <svg
     fill="none"
     height="24"
@@ -11,11 +11,30 @@ const InfoCircleIcon = `export const InfoCircleIcon = (props) => {
       d="M12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22ZM12.75 16C12.75 16.41 12.41 16.75 12 16.75C11.59 16.75 11.25 16.41 11.25 16L11.25 11C11.25 10.59 11.59 10.25 12 10.25C12.41 10.25 12.75 10.59 12.75 11L12.75 16ZM11.08 7.62C11.13 7.49 11.2 7.39 11.29 7.29C11.39 7.2 11.5 7.13 11.62 7.08C11.74 7.03 11.87 7 12 7C12.13 7 12.26 7.03 12.38 7.08C12.5 7.13 12.61 7.2 12.71 7.29C12.8 7.39 12.87 7.49 12.92 7.62C12.97 7.74 13 7.87 13 8C13 8.13 12.97 8.26 12.92 8.38C12.87 8.5 12.8 8.61 12.71 8.71C12.61 8.8 12.5 8.87 12.38 8.92C12.14 9.02 11.86 9.02 11.62 8.92C11.5 8.87 11.39 8.8 11.29 8.71C11.2 8.61 11.13 8.5 11.08 8.38C11.03 8.26 11 8.13 11 8C11 7.87 11.03 7.74 11.08 7.62Z"
     />
   </svg>
-};`;
+);`;
+
+const CloseIcon = `export const CloseIcon = (props) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="1em"
+    role="presentation"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    width="1em"
+  >
+    <path d="M18 6L6 18M6 6l12 12" />
+  </svg>
+);`;
 
 const App = `import React, {forwardRef, useMemo} from "react";
 import {useAlert} from "@nextui-org/react";
 import {InfoCircleIcon} from "./InfoCircleIcon";
+import {CloseIcon} from "./CloseIcon"
 
 const styles = {
   base: [
@@ -84,7 +103,7 @@ const MyAlert = forwardRef((props, ref) => {
         {mainWrapper}
         {(isClosable || onClose) && (
           <button onClick={handleClose} {...getCloseButtonProps()}>
-            <AlertCloseIcon color={color} />
+            <CloseIcon />
           </button>
         )}
       </div>
@@ -111,6 +130,7 @@ export default MyAlert;`;
 const react = {
   "/App.jsx": App,
   "/InfoCircleIcon": InfoCircleIcon,
+  "/CloseIcon": CloseIcon,
 };
 
 export default {
