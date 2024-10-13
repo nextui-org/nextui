590587f1de664111f452b709b24e358c9ef6b6bf
doki- <1335902682@qq.com>
2024-10-13 23:21:21 +0800

    feat(textarea): delete the px classname

--- packages/components/input/src/use-input.ts
+++ packages/components/input/src/use-input.ts
@@ -370,11 +370,7 @@ export function useInput<T extends HTMLInputElement | HTMLTextAreaElement = HTML
         "data-has-start-content": dataAttr(hasStartContent),
         "data-has-end-content": dataAttr(!!endContent),
         className: slots.input({
-          class: clsx(
-            classNames?.input,
-            isFilled ? "is-filled" : "",
-            isMultiline ? "px-0 pe-0" : "",
-          ),
+          class: clsx(classNames?.input, isFilled ? "is-filled" : "", isMultiline ? "pe-0" : ""),
         }),
         ...mergeProps(
           focusProps,
