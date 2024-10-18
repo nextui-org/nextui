c9c165c868a3a39742cfea47eabfd7e6b67e681a
Filip Frincu <filipfrincu@gmail.com>
2024-10-18 21:36:44 +0300

    fix: render theme builder only on client

--- apps/docs/components/themes/components/showcase/circular-progress.tsx
+++ apps/docs/components/themes/components/showcase/circular-progress.tsx
@@ -6,12 +6,24 @@ import {ShowcaseComponent} from "../showcase-component";
 export function CircularProgress() {
   return (
     <ShowcaseComponent defaultVariant="solid" name="CircularProgress" sizes={sizes}>
-      <NextUICircularProgress color="default">Default</NextUICircularProgress>
-      <NextUICircularProgress color="primary">Primary</NextUICircularProgress>
-      <NextUICircularProgress color="secondary">Secondary</NextUICircularProgress>
-      <NextUICircularProgress color="success">Success</NextUICircularProgress>
-      <NextUICircularProgress color="warning">Warning</NextUICircularProgress>
-      <NextUICircularProgress color="danger">Danger</NextUICircularProgress>
+      <NextUICircularProgress aria-label="default" color="default">
+        Default
+      </NextUICircularProgress>
+      <NextUICircularProgress aria-label="primary" color="primary">
+        Primary
+      </NextUICircularProgress>
+      <NextUICircularProgress aria-label="secondary" color="secondary">
+        Secondary
+      </NextUICircularProgress>
+      <NextUICircularProgress aria-label="success" color="success">
+        Success
+      </NextUICircularProgress>
+      <NextUICircularProgress aria-label="warning" color="warning">
+        Warning
+      </NextUICircularProgress>
+      <NextUICircularProgress aria-label="danger" color="danger">
+        Danger
+      </NextUICircularProgress>
     </ShowcaseComponent>
   );
 }
