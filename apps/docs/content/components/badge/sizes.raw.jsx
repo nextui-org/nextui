5d7b2c88f6e9681f4ce0f065a52f82bce2390ea9
WK Wong <wingkwong.code@gmail.com>
2024-10-29 23:53:44 +0800

    refactor(docs): badge dx

---
+++ apps/docs/content/components/badge/sizes.raw.jsx
@@ -0,0 +1,17 @@
+import {Badge, Avatar} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex gap-3 items-center">
+      <Badge color="primary" content="5" size="sm">
+        <Avatar radius="md" src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+      </Badge>
+      <Badge color="primary" content="5" size="md">
+        <Avatar radius="md" src="https://i.pravatar.cc/300?u=a042581f4e29026709d" />
+      </Badge>
+      <Badge color="primary" content="5" size="lg">
+        <Avatar radius="md" src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+      </Badge>
+    </div>
+  );
+}
