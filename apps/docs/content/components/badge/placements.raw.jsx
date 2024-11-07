2dc2bcd83ca3592e7157676dd9e207e21206bbd6
աӄա <wingkwong.code@gmail.com>
2024-11-07 21:21:46 +0800

    refactor(docs): badge dx (#3960)
    * refactor(docs): badge dx
    
    * chore(docs): incorrect import path

---
+++ apps/docs/content/components/badge/placements.raw.jsx
@@ -0,0 +1,20 @@
+import {Badge, Avatar} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex gap-4 items-center">
+      <Badge color="danger" content="5" placement="top-right">
+        <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a042f81f4e29026024d" />
+      </Badge>
+      <Badge color="danger" content="5" placement="bottom-right">
+        <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+      </Badge>
+      <Badge color="danger" content="5" placement="top-left">
+        <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+      </Badge>
+      <Badge color="danger" content="5" placement="bottom-left">
+        <Avatar isBordered radius="md" src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+      </Badge>
+    </div>
+  );
+}
