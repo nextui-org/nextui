17c14198b0b287ab67786e469970ed3cf6ec5b2a
աӄա <wingkwong.code@gmail.com>
2024-11-07 15:07:47 +0800

    refactor(docs): breadcrumbs dx (#3968)
    * refactor(docs): breadcrumbs dx
    
    * fix(docs): export issue
    
    * chore(docs): use preserve for jsx
    
    * fix(docs): support multiline import
    
    * fix(docs): support multiple export
    
    * chore(docs): add back export

---
+++ apps/docs/content/components/breadcrumbs/variants.raw.jsx
@@ -0,0 +1,19 @@
+import {Breadcrumbs, BreadcrumbItem} from "@nextui-org/react";
+
+export default function App() {
+  const variants = ["solid", "bordered", "light"];
+
+  return (
+    <div className="flex flex-col flex-wrap gap-4">
+      {variants.map((variant) => (
+        <Breadcrumbs key={variant} variant={variant}>
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Music</BreadcrumbItem>
+          <BreadcrumbItem>Artist</BreadcrumbItem>
+          <BreadcrumbItem>Album</BreadcrumbItem>
+          <BreadcrumbItem>Song</BreadcrumbItem>
+        </Breadcrumbs>
+      ))}
+    </div>
+  );
+}
