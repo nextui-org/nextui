62a6e748fbd533a0d21e7d864ffe949f452636c5
WK Wong <wingkwong.code@gmail.com>
2024-11-10 23:32:55 +0800

    refactor(docs): checkbox dx

--- apps/docs/content/components/checkbox/radius.ts
+++ apps/docs/content/components/checkbox/radius.ts
@@ -1,16 +1,4 @@
-const App = `import {Checkbox} from "@nextui-org/react";
-
-export default function App() {
-  return (
-    <div className="flex gap-4">
-      <Checkbox defaultSelected radius="full">Full</Checkbox>
-      <Checkbox defaultSelected radius="lg">Large</Checkbox>
-      <Checkbox defaultSelected radius="md">Medium</Checkbox>
-      <Checkbox defaultSelected radius="sm">Small</Checkbox>
-      <Checkbox defaultSelected radius="none">None</Checkbox>
-    </div>  
-  );
-}`;
+import App from "./radius.raw.jsx?raw";
 
 const react = {
   "/App.jsx": App,
