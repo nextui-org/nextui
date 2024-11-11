433d317cc2f27bccfbe7e59b4a801c8974a4b45e
WK Wong <wingkwong.code@gmail.com>
2024-11-11 19:57:01 +0800

    refactor(docs): circular-progress dx

--- apps/docs/content/components/circular-progress/value.ts
+++ apps/docs/content/components/circular-progress/value.ts
@@ -1,26 +1,4 @@
-const App = `import {CircularProgress} from "@nextui-org/react";
-
-export default function App() {
-  const [value, setValue] = React.useState(0);
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setValue((v) => (v >= 100 ? 0 : v + 10));
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <CircularProgress
-      aria-label="Loading..."
-      size="lg"
-      value={value}
-      color="warning"
-      showValueLabel={true}
-    />
-  );
-}`;
+import App from "./value.raw.jsx?raw";
 
 const react = {
   "/App.jsx": App,
