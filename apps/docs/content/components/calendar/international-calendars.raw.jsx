473b40b62daf05033bffd1f1f2f890be36286f6c
աӄա <wingkwong.code@gmail.com>
2024-11-14 04:29:22 +0800

    refactor(docs): calendar dx (#4022)
    * refactor(docs): calendar dx
    
    * fix(docs): incorrect import path

---
+++ apps/docs/content/components/calendar/international-calendars.raw.jsx
@@ -0,0 +1,10 @@
+import {Calendar} from "@nextui-org/react";
+import {I18nProvider} from "@react-aria/i18n";
+
+export default function App() {
+  return (
+    <I18nProvider locale="zh-CN-u-ca-chinese">
+      <Calendar aria-label="Date (International Calendar)" />
+    </I18nProvider>
+  );
+}
