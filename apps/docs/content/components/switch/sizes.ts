0b9fd367c491e8ac5cd9c077432c25b314818a88
աӄա <wingkwong.code@gmail.com>
2024-11-14 04:29:46 +0800

    refactor(docs): switch dx (#4037)
    * refactor(docs): switch dx
    
    * chore(docs): remove highlightedLines

--- apps/docs/content/components/switch/sizes.ts
+++ apps/docs/content/components/switch/sizes.ts
@@ -1,14 +1,4 @@
-const App = `import {Switch} from "@nextui-org/react";
-
-export default function App() {
-  return (
-    <div className="flex gap-4">
-      <Switch defaultSelected size="sm">Small</Switch>
-      <Switch defaultSelected size="md">Medium</Switch>
-      <Switch defaultSelected size="lg">Large</Switch>
-    </div>  
-  );
-}`;
+import App from "./sizes.raw.jsx?raw";
 
 const react = {
   "/App.jsx": App,
