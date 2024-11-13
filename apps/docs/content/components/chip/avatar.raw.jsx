565197de81ce4fb7e72aca7cc339819051fd10af
աӄա <wingkwong.code@gmail.com>
2024-11-14 04:31:02 +0800

    refactor(docs): chip-dx (#4028)

---
+++ apps/docs/content/components/chip/avatar.raw.jsx
@@ -0,0 +1,20 @@
+import {Chip, Avatar} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex gap-4">
+      <Chip
+        avatar={<Avatar name="JW" src="https://i.pravatar.cc/300?u=a042581f4e29026709d" />}
+        variant="flat"
+      >
+        Avatar
+      </Chip>
+      <Chip
+        avatar={<Avatar getInitials={(name) => name.charAt(0)} name="JW" size="sm" />}
+        variant="flat"
+      >
+        Avatar
+      </Chip>
+    </div>
+  );
+}
---
+++ apps/docs/content/components/chip/colors.raw.jsx
@@ -0,0 +1,14 @@
+import {Chip} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex gap-4">
+      <Chip color="default">Default</Chip>
+      <Chip color="primary">Primary</Chip>
+      <Chip color="secondary">Secondary</Chip>
+      <Chip color="success">Success</Chip>
+      <Chip color="warning">Warning</Chip>
+      <Chip color="danger">Danger</Chip>
+    </div>
+  );
+}
