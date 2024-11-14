46c3c396ebeefa91282ab03d9f75dc6c66b755c7
WK Wong <wingkwong.code@gmail.com>
2024-11-15 00:40:06 +0800

    refactor(docs): kbd dx

---
+++ apps/docs/content/components/kbd/keys.raw.jsx
@@ -0,0 +1,11 @@
+import {Kbd} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex gap-4">
+      <Kbd keys={["command"]}>K</Kbd>
+      <Kbd keys={["command", "shift"]}>N</Kbd>
+      <Kbd keys={["option", "command"]}>P</Kbd>
+    </div>
+  );
+}
---
+++ apps/docs/content/components/kbd/usage.raw.jsx
@@ -0,0 +1,5 @@
+import {Kbd} from "@nextui-org/react";
+
+export default function App() {
+  return <Kbd keys={["command"]}>K</Kbd>;
+}
