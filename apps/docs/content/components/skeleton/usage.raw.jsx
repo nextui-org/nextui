6f3731109c1ab16658545eea4781b194ec2a43c7
աӄա <wingkwong.code@gmail.com>
2024-11-15 03:04:57 +0800

    refactor(docs): skeleton dx (#4042)

---
+++ apps/docs/content/components/skeleton/usage.raw.jsx
@@ -0,0 +1,22 @@
+import {Card, Skeleton} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <Card className="w-[200px] space-y-5 p-4" radius="lg">
+      <Skeleton className="rounded-lg">
+        <div className="h-24 rounded-lg bg-default-300" />
+      </Skeleton>
+      <div className="space-y-3">
+        <Skeleton className="w-3/5 rounded-lg">
+          <div className="h-3 w-3/5 rounded-lg bg-default-200" />
+        </Skeleton>
+        <Skeleton className="w-4/5 rounded-lg">
+          <div className="h-3 w-4/5 rounded-lg bg-default-200" />
+        </Skeleton>
+        <Skeleton className="w-2/5 rounded-lg">
+          <div className="h-3 w-2/5 rounded-lg bg-default-300" />
+        </Skeleton>
+      </div>
+    </Card>
+  );
+}
