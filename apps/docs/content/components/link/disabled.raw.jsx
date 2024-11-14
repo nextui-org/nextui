bfcc75c3da16a8f14a95fb3c027aff0d43e34dbc
աӄա <wingkwong.code@gmail.com>
2024-11-15 03:07:29 +0800

    refactor(docs): link dx (#4048)

---
+++ apps/docs/content/components/link/disabled.raw.jsx
@@ -0,0 +1,9 @@
+import {Link} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <Link isDisabled href="#">
+      Disabled Link
+    </Link>
+  );
+}
--- apps/docs/content/components/link/underline.ts
+++ apps/docs/content/components/link/underline.ts
@@ -1,16 +1,4 @@
-const App = `import {Link} from "@nextui-org/react";
-
-export default function App() {
-  return (
-    <div className="flex gap-4">
-      <Link href="#" underline="none">None</Link>
-      <Link href="#" underline="hover">Hover</Link>
-      <Link href="#" underline="always">Always</Link>
-      <Link href="#" underline="active">Active</Link>
-      <Link href="#" underline="focus">Focus</Link>
-    </div>  
-  );
-}`;
+import App from "./underline.raw.jsx?raw";
 
 const react = {
   "/App.jsx": App,
