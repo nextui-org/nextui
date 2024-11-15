6af49558d17868fca9a78a8d42e058b44a231dd9
WK Wong <wingkwong.code@gmail.com>
2024-11-15 19:04:36 +0800

    refactor(docs): pagination dx

---
+++ apps/docs/content/components/pagination/loop.raw.jsx
@@ -0,0 +1,5 @@
+import {Pagination} from "@nextui-org/react";
+
+export default function App() {
+  return <Pagination loop showControls color="success" initialPage={1} total={5} />;
+}
---
+++ apps/docs/content/components/pagination/sizes.raw.jsx
@@ -0,0 +1,13 @@
+import {Pagination} from "@nextui-org/react";
+
+export default function App() {
+  const sizes = ["sm", "md", "lg"];
+
+  return (
+    <div className="flex flex-wrap gap-4 items-center">
+      {sizes.map((size) => (
+        <Pagination key={size} initialPage={1} size={size} total={10} />
+      ))}
+    </div>
+  );
+}
