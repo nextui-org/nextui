fea432b7eff615620122621934c0ac105ae7f2bc
WK Wong <wingkwong.code@gmail.com>
2024-11-17 00:03:35 +0800

    refactor(docs): spinner dx

---
+++ apps/docs/content/components/spinner/label-colors.raw.jsx
@@ -0,0 +1,14 @@
+import {Spinner} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="flex gap-4">
+      <Spinner color="default" label="Default" labelColor="foreground" />
+      <Spinner color="primary" label="Primary" labelColor="primary" />
+      <Spinner color="secondary" label="Secondary" labelColor="secondary" />
+      <Spinner color="success" label="Success" labelColor="success" />
+      <Spinner color="warning" label="Warning" labelColor="warning" />
+      <Spinner color="danger" label="Danger" labelColor="danger" />
+    </div>
+  );
+}
