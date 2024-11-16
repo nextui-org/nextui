59a5644e2c8523b40dc9a0b5ec9d81b027b3ce62
Junior Garcia <jrgarciadev@gmail.com>
2024-11-15 21:30:12 -0300

    feat: alert styles improved

--- packages/components/alert/stories/alert.stories.tsx
+++ packages/components/alert/stories/alert.stories.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {Meta} from "@storybook/react";
-import {alert} from "@nextui-org/theme";
+import {alert, cn} from "@nextui-org/theme";
 import {Button} from "@nextui-org/button";
 
-import {Alert} from "../src";
+import {Alert, AlertProps} from "../src";
 
 export default {
   title: "Components/Alert",
@@ -120,57 +120,94 @@ const WithEndContentTemplate = (args) => {
   );
 };
 
-export const CustomStylesTemplate = (args) => {
-  const [isVisible, setIsVisible] = React.useState(true);
+const CustomAlert = React.forwardRef<HTMLDivElement, AlertProps>(
+  (
+    {title, children, variant = "faded", color = "secondary", className, classNames, ...props},
+    ref,
+  ) => {
+    const colorClass = React.useMemo(() => {
+      switch (color) {
+        case "default":
+          return "before:bg-default-300";
+        case "primary":
+          return "before:bg-primary";
+        case "secondary":
+          return "before:bg-secondary";
+        case "success":
+          return "before:bg-success";
+        case "warning":
+          return "before:bg-warning";
+        case "danger":
+          return "before:bg-danger";
+        default:
+          return `before:bg-default-200`;
+      }
+    }, []);
+
+    return (
+      <Alert
+        ref={ref}
+        classNames={{
+          ...classNames,
+          base: cn(
+            [
+              "bg-default-50 dark:bg-background shadow-sm",
+              "border-1 border-default-200 dark:border-default-100",
+              "relative before:content-[''] before:absolute before:z-10",
+              "before:left-0 before:top-[-1px] before:bottom-[-1px] before:w-1",
+              "rounded-l-none border-l-0",
+              colorClass,
+            ],
+            classNames?.base,
+            className,
+          ),
+          mainWrapper: cn("pt-1", classNames?.mainWrapper),
+          iconWrapper: cn("dark:bg-transparent", classNames?.iconWrapper),
+        }}
+        color={color}
+        title={title}
+        variant={variant}
+        {...props}
+      >
+        <div className="flex items-center gap-1 mt-3">{children}</div>
+      </Alert>
+    );
+  },
+);
+
+CustomAlert.displayName = "CustomAlert";
+
+const CustomStylesTemplate = (args) => {
+  const colors = ["default", "primary", "secondary", "success", "warning", "danger"];
 
   return (
-    <>
-      {!isVisible && (
-        <Button
-          className="bg-background text-default-700 font-medium border-1 shadow-small"
-          size="sm"
-          variant="bordered"
-          onPress={() => setIsVisible(true)}
+    <div className="flex flex-col w-full gap-y-6">
+      {colors.map((color) => (
+        <CustomAlert
+          {...args}
+          key={color}
+          color={color}
+          title="The documents you requested are ready to be viewed"
         >
-          Show Alert
-        </Button>
-      )}
-      <Alert
-        {...args}
-        classNames={{
-          base: [
-            "bg-default-50 dark:bg-background",
-            "relative before:content-[''] before:absolute before:z-10",
-            "before:left-0 before:top-[-1px] before:bottom-[-1px] before:w-1 before:bg-secondary",
-            "rounded-l-none border-l-0",
-          ],
-          mainWrapper: "pt-1",
-          iconWrapper: "border-1 border-secondary-200 dark:bg-transparent",
-          alertIcon: "text-secondary",
-        }}
-        isVisible={isVisible}
-        title="The documents you requested are ready to be viewed"
-        variant="faded"
-        onClose={() => setIsVisible(false)}
-      >
-        <div className="flex items-center gap-1 mt-3">
-          <Button
-            className="bg-background text-default-700 font-medium border-1 shadow-small"
-            size="sm"
-            variant="bordered"
-          >
-            View documents
-          </Button>
-          <Button
-            className="text-default-500 font-medium underline underline-offset-4"
-            size="sm"
-            variant="light"
-          >
-            Maybe later
-          </Button>
-        </div>
-      </Alert>
-    </>
+          <div className="flex items-center gap-1 mt-3">
+            <Button
+              className="bg-background text-default-700 font-medium border-1 shadow-small"
+              size="sm"
+              variant="bordered"
+            >
+              View documents
+            </Button>
+            <Button
+              className="text-default-500 font-medium underline underline-offset-4"
+              size="sm"
+              variant="light"
+            >
+              Maybe later
+            </Button>
+          </div>
+        </CustomAlert>
+      ))}
+    </div>
   );
 };
 
