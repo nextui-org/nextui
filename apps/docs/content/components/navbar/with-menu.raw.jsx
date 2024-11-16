b69819348b582376c0a5983d5aafc3b37c6066b0
WK Wong <wingkwong.code@gmail.com>
2024-11-16 12:49:21 +0800

    refactor(docs): navbar dx

--- apps/docs/content/components/navbar/with-menu.raw.jsx
+++ apps/docs/content/components/navbar/with-menu.raw.jsx
@@ -10,16 +10,18 @@ import {
   Button,
 } from "@nextui-org/react";
 
-export const AcmeLogo = () => (
-  <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-    <path
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="currentColor"
-      fillRule="evenodd"
-    />
-  </svg>
-);
+export const AcmeLogo = () => {
+  return (
+    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+      <path
+        clipRule="evenodd"
+        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
