4c4fb11e03c081504662a27711247abaf714a9c9
WK Wong <wingkwong.code@gmail.com>
2024-11-16 12:56:27 +0800

    refactor(docs): select dx

--- apps/docs/content/components/select/custom-selector-icon.raw.jsx
+++ apps/docs/content/components/select/custom-selector-icon.raw.jsx
@@ -1,25 +1,27 @@
 import {Select, SelectItem} from "@nextui-org/react";
 
-export const SelectorIcon = (props) => (
-  <svg
-    aria-hidden="true"
-    fill="none"
-    focusable="false"
-    height="1em"
-    role="presentation"
-    stroke="currentColor"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="1.5"
-    viewBox="0 0 24 24"
-    width="1em"
-    {...props}
-  >
-    <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-    <path d="M8 9l4 -4l4 4" />
-    <path d="M16 15l-4 4l-4 -4" />
-  </svg>
-);
+export const SelectorIcon = (props) => {
+  return (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height="1em"
+      role="presentation"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      width="1em"
+      {...props}
+    >
+      <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+      <path d="M8 9l4 -4l4 4" />
+      <path d="M16 15l-4 4l-4 -4" />
+    </svg>
+  );
+};
 
 export const animals = [
   {key: "cat", label: "Cat"},
