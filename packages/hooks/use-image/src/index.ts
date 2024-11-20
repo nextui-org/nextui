b521aea5f8d2d36a85dfb8de9b1c51d181324559
WK Wong <wingkwong.code@gmail.com>
2024-11-20 16:49:32 +0800

    fix(use-image): image ReferenceError in SSR

--- packages/hooks/use-image/src/index.ts
+++ packages/hooks/use-image/src/index.ts
@@ -122,7 +122,7 @@ function setImageAndGetInitialStatus(
   if (!src) return "pending";
   if (ignoreFallback) return "loaded";
 
-  const img = new Image();
+  const img = document.createElement("img");
 
   img.src = src;
   if (crossOrigin) img.crossOrigin = crossOrigin;
