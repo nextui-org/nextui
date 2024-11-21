7ffedaa46efae00061edc7f944a4f27a2b85a61d
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-11-21 23:39:51 +0900

    chore(docs): replace the props of autocomplete from value to key

--- apps/docs/content/components/autocomplete/usage.raw.jsx
+++ apps/docs/content/components/autocomplete/usage.raw.jsx
@@ -1,31 +1,31 @@
 import {Autocomplete, AutocompleteItem} from "@nextui-org/react";
 
 export const animals = [
-  {label: "Cat", value: "cat", description: "The second most popular pet in the world"},
-  {label: "Dog", value: "dog", description: "The most popular pet in the world"},
-  {label: "Elephant", value: "elephant", description: "The largest land animal"},
-  {label: "Lion", value: "lion", description: "The king of the jungle"},
-  {label: "Tiger", value: "tiger", description: "The largest cat species"},
-  {label: "Giraffe", value: "giraffe", description: "The tallest land animal"},
+  {label: "Cat", key: "cat", description: "The second most popular pet in the world"},
+  {label: "Dog", key: "dog", description: "The most popular pet in the world"},
+  {label: "Elephant", key: "elephant", description: "The largest land animal"},
+  {label: "Lion", key: "lion", description: "The king of the jungle"},
+  {label: "Tiger", key: "tiger", description: "The largest cat species"},
+  {label: "Giraffe", key: "giraffe", description: "The tallest land animal"},
   {
     label: "Dolphin",
-    value: "dolphin",
+    key: "dolphin",
     description: "A widely distributed and diverse group of aquatic mammals",
   },
-  {label: "Penguin", value: "penguin", description: "A group of aquatic flightless birds"},
-  {label: "Zebra", value: "zebra", description: "A several species of African equids"},
+  {label: "Penguin", key: "penguin", description: "A group of aquatic flightless birds"},
+  {label: "Zebra", key: "zebra", description: "A several species of African equids"},
   {
     label: "Shark",
-    value: "shark",
+    key: "shark",
     description: "A group of elasmobranch fish characterized by a cartilaginous skeleton",
   },
   {
     label: "Whale",
-    value: "whale",
+    key: "whale",
     description: "Diverse group of fully aquatic placental marine mammals",
   },
-  {label: "Otter", value: "otter", description: "A carnivorous mammal in the subfamily Lutrinae"},
-  {label: "Crocodile", value: "crocodile", description: "A large semiaquatic reptile"},
+  {label: "Otter", key: "otter", description: "A carnivorous mammal in the subfamily Lutrinae"},
+  {label: "Crocodile", key: "crocodile", description: "A large semiaquatic reptile"},
 ];
 
 export default function App() {
@@ -33,9 +33,7 @@ export default function App() {
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Autocomplete className="max-w-xs" label="Select an animal">
         {animals.map((animal) => (
-          <AutocompleteItem key={animal.value} value={animal.value}>
-            {animal.label}
-          </AutocompleteItem>
+          <AutocompleteItem key={animal.key}>{animal.label}</AutocompleteItem>
         ))}
       </Autocomplete>
       <Autocomplete
@@ -44,7 +42,7 @@ export default function App() {
         label="Favorite Animal"
         placeholder="Search an animal"
       >
-        {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
+        {(item) => <AutocompleteItem key={item.key}>{item.label}</AutocompleteItem>}
       </Autocomplete>
     </div>
   );
