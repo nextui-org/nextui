2d0e08d3a67db25252f23fc2c272f06f4ba013e7
winches <329487092@qq.com>
2024-11-24 21:32:06 +0800

    chore: remove @mdx-js/react optimizeDep

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["@storybook/theming", "@mdx-js/react"],
+    include: ["@storybook/theming"],
   },
 });
