db383c27911e8999aac2cb6d4d6110069536d77d
աӄա <wingkwong.code@gmail.com>
2024-11-28 02:05:39 +0800

    refactor(docs): tabs dx (#4067)
    * refactor(docs): tab dx
    
    * refactor(docs): tabs dx

--- apps/docs/content/components/tabs/colors.ts
+++ apps/docs/content/components/tabs/colors.ts
@@ -1,27 +1,4 @@
-const App = `import {Tabs, Tab} from "@nextui-org/react";
-
-export default function App() {
-  const colors = [
-    "default",
-    "primary",
-    "secondary",
-    "success",
-    "warning",
-    "danger"
-  ];
-
-  return (
-    <div className="flex flex-wrap gap-4">
-      {colors.map((color) => (
-        <Tabs key={color} color={color} aria-label="Tabs colors" radius="full">
-          <Tab key="photos" title="Photos"/>
-          <Tab key="music" title="Music"/>
-          <Tab key="videos" title="Videos"/>
-        </Tabs>
-      ))}
-    </div>
-  );
-}`;
+import App from "./colors.raw.jsx?raw";
 
 const react = {
   "/App.jsx": App,
