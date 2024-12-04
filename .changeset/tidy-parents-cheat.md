adbd85ee95b67ee779401307c49596dd45e062fc
Peterl561 <76144929+Peterl561@users.noreply.github.com>
2024-12-04 14:28:03 -0800

    fix(pagination): cursor position when hidden on init (#4222)
    * fix(pagination): cusor position when hidden on init
    
    * test(pagination): cursor intersection observer
    
    * chore(changeset): pagination cursor position fix
    
    * refactor(pagination): minor nitpicks
    - check for null ref in usePagination
    - restore original IntersectionObserver in test

---
+++ .changeset/tidy-parents-cheat.md
@@ -0,0 +1,5 @@
+---
+"@nextui-org/pagination": patch
+---
+
+fix pagination cursor position on first load
--- packages/components/pagination/__tests__/pagination.test.tsx
+++ packages/components/pagination/__tests__/pagination.test.tsx
@@ -79,4 +79,78 @@ describe("Pagination", () => {
 
     expect(cursor).toHaveAttribute("aria-hidden", "true");
   });
+
+  it("the pagination cursor should not have data-moving attribute before intersection", () => {
+    const originalIntersectionObserver = window.IntersectionObserver;
+
+    // save callback and options to later emulate intersection event
+    let intersectCallback: IntersectionObserverCallback | undefined;
+    let intersectOptions: IntersectionObserverInit | undefined;
+
+    // defined here as we need the closure
+    class MockIntersectionObserver implements IntersectionObserver {
+      root: Element | null = null;
+      rootMargin: string = "";
+      thresholds: number[] = [];
+      disconnect: () => void = () => {};
+      observe: (target: Element) => void = () => {};
+      unobserve: (target: Element) => void = () => {};
+      takeRecords: () => IntersectionObserverEntry[] = () => [];
+
+      constructor(
+        private readonly callback: IntersectionObserverCallback,
+        private readonly options?: IntersectionObserverInit,
+      ) {
+        this.root = (this.options?.root ?? null) as Element | null;
+        this.rootMargin = this.options?.rootMargin ?? "";
+        this.thresholds = Array.isArray(this.options?.threshold)
+          ? this.options.threshold
+          : this.options?.threshold != null
+          ? [this.options.threshold]
+          : [0];
+        this.disconnect = jest.fn();
+        this.observe = jest.fn();
+        this.unobserve = jest.fn();
+        this.takeRecords = jest.fn();
+
+        // save to closure
+        intersectCallback = this.callback;
+        intersectOptions = this.options;
+      }
+    }
+
+    // hijack the IntersectionObserver implementation so useIntersectionObserver returns our mock
+    window.IntersectionObserver = MockIntersectionObserver;
+
+    const {container, rerender} = render(<Pagination total={10} />);
+
+    const cursor = container.querySelector("span[data-slot='cursor']");
+
+    // on first render, the cursor should not have the data-moving attribute until the observer is triggered
+    expect(cursor).not.toHaveAttribute("data-moving");
+
+    // trigger the observer with a full intersection
+    intersectCallback?.(
+      [
+        {
+          isIntersecting: true,
+          intersectionRatio: 1,
+          target: container,
+          boundingClientRect: {} as DOMRectReadOnly,
+          intersectionRect: {} as DOMRectReadOnly,
+          rootBounds: {} as DOMRectReadOnly,
+          time: 0,
+        },
+      ],
+      new MockIntersectionObserver(intersectCallback, intersectOptions),
+    );
+
+    // rerender the component to update cursor state after intersection
+    rerender(<Pagination total={10} />);
+
+    // on rerender, the cursor should have the data-moving attribute
+    expect(cursor).toHaveAttribute("data-moving");
+
+    window.IntersectionObserver = originalIntersectionObserver;
+  });
 });
