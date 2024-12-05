e1480504dea9489585de51967d8e0e796e01f08c
Junior Garcia <jrgarciadev@gmail.com>
2024-12-04 22:53:57 -0300

    chore: form in progress

---
+++ apps/docs/content/components/form/controlled.raw.jsx
@@ -0,0 +1,38 @@
+import {Form, Input, Button} from "@nextui-org/react";
+
+export default function App() {
+  const [email, setEmail] = React.useState("");
+  const [submitted, setSubmitted] = React.useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    setSubmitted(data);
+  };
+
+  return (
+    <Form className="w-full max-w-xs" validationBehavior="native" onSubmit={onSubmit}>
+      <Input
+        isRequired
+        errorMessage="Please enter a valid email"
+        label="Email"
+        labelPlacement="outside"
+        name="email"
+        placeholder="Enter your email"
+        type="email"
+        value={email}
+        onValueChange={setEmail}
+      />
+      <Button type="submit" variant="bordered">
+        Submit
+      </Button>
+      {submitted && (
+        <div className="text-small text-default-500">
+          You submitted: <code>{JSON.stringify(submitted)}</code>
+        </div>
+      )}
+    </Form>
+  );
+}
--- apps/docs/content/components/form/index.ts
+++ apps/docs/content/components/form/index.ts
@@ -1,5 +1,13 @@
 import usage from "./usage";
+import controlled from "./controlled";
+import nativeValidation from "./native-validation";
+import customErrorMessages from "./custom-error-messages";
+import customValidation from "./custom-validation";
 
 export const formContent = {
   usage,
+  controlled,
+  nativeValidation,
+  customErrorMessages,
+  customValidation,
 };
