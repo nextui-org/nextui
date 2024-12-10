9fb54c25471bf0acdc303b4ed3c9e2c5acb90d2e
Omar Shehab <31727466+omarshehab221@users.noreply.github.com>
2024-12-10 12:58:39 +0200

    export Drawer children props

--- packages/components/drawer/src/index.ts
+++ packages/components/drawer/src/index.ts
@@ -1,9 +1,26 @@
-import {ModalHeader, ModalBody, ModalFooter, ModalContent} from "@nextui-org/modal";
+import {
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalContent,
+  ModalContentProps,
+  ModalHeaderProps,
+  ModalBodyProps,
+  ModalFooterProps,
+  UseDisclosureProps,
+} from "@nextui-org/modal";
 
 import Drawer from "./drawer";
 
 // export types
 export type {DrawerProps} from "./drawer";
+export type {
+  ModalContentProps as DrawerContentProps,
+  ModalHeaderProps as DrawerHeaderProps,
+  ModalBodyProps as DrawerBodyProps,
+  ModalFooterProps as DrawerFooterProps,
+  UseDisclosureProps,
+}
 
 // export hooks
 export {useDrawer} from "./use-drawer";
