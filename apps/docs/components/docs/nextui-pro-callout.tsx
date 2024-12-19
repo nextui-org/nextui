281f6b9e642cc1dc60a7970d1ef53301ce974db3
Junior Garcia <jrgarciadev@gmail.com>
2024-12-19 09:26:49 -0300

    chore: adjustments

--- apps/docs/components/docs/nextui-pro-callout.tsx
+++ apps/docs/components/docs/nextui-pro-callout.tsx
@@ -16,7 +16,7 @@ export const NextUIProCallout = () => {
   return (
     <div className="relative w-full max-w-[12rem] flex flex-col items-center border border-default/60 hover:border-default/90 rounded-xl py-6 px-2 cursor-pointer">
       <div>
-        <p className="leading-tight tracking-tight text-center text-xl font-semibold">
+        <p className="leading-[1.025] tracking-tight text-center text-large font-semibold">
           Ship&nbsp;
           <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#5EA2EF] to-[#0072F5]">
             faster
@@ -26,11 +26,11 @@ export const NextUIProCallout = () => {
           <br />
           components
         </p>
-        <p className="text-center text-xs mt-2 px-6 font-medium text-default-500 leading-tight">
-          Discover 210+ stunning, responsive components crafted by the NextUI team.
+        <p className="text-center text-xs mt-2 px-3 font-medium text-default-500 dark:text-default-400 leading-tight">
+          Discover 210+ stunning components by NextUI
         </p>
       </div>
-      <div className="mt-2 w-fit flex group items-center text-foreground hover:shadow-sm relative overflow-hidden rounded-full p-[2px]">
+      <div className="mt-3 w-fit flex group items-center text-foreground hover:shadow-sm relative overflow-hidden rounded-full p-[2px]">
         <span className="absolute inset-[-1000%] bg-[conic-gradient(from_90deg_at_50%_50%,#338EF7_0%,#F54180_50%,#338EF7_100%)]" />
         <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background transition-background p-2.5 text-xs font-medium hover:font-semibold text-foreground backdrop-blur-3xl">
           Explore Components
