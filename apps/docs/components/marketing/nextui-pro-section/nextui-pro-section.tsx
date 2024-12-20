7b38e04fee196d797ce9fef0a9e1150b2177cb11
Junior Garcia <jrgarciadev@gmail.com>
2024-12-20 10:43:02 -0300

    chore: hide pro image on mobile

--- apps/docs/components/marketing/nextui-pro-section/nextui-pro-section.tsx
+++ apps/docs/components/marketing/nextui-pro-section/nextui-pro-section.tsx
@@ -87,7 +87,7 @@ export const NextUIProSection = () => {
       <div className="hidden md:block w-screen">
         <Marquee
           vertical
-          className="relative flex overflow-visible items-center min-h-[480px]"
+          className="hidden md:block relative flex overflow-visible items-center min-h-[480px]"
           duration={90}
           wrapperClassName="absolute md:top-24 -left-[47%] w-[1000px]"
         >
