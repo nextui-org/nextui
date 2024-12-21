5b5599127fd31b521b226a2a00f5f29961a6bc29
WK Wong <wingkwong.code@gmail.com>
2024-12-21 23:30:10 +0800

    feat(shared-icons): add ChevronLeftIcon

---
+++ packages/utilities/shared-icons/src/chevron-left.tsx
@@ -0,0 +1,20 @@
+import {IconSvgProps} from "./types";
+
+export const ChevronLeftIcon = (props: IconSvgProps) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="1em"
+    role="presentation"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="1.5"
+    viewBox="0 0 24 24"
+    width="1em"
+    {...props}
+  >
+    <path d="m15 18-6-6 6-6" />
+  </svg>
+);
