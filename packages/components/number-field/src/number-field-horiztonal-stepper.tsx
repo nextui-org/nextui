9a9cb4651a6e751a2032b2b13a57e5c1e0f0c324
WK Wong <wingkwong.code@gmail.com>
2024-12-21 23:29:53 +0800

    refactor(number-field): revise stepper icons

--- packages/components/number-field/src/number-field-horiztonal-stepper.tsx
+++ packages/components/number-field/src/number-field-horiztonal-stepper.tsx
@@ -2,6 +2,7 @@ import type {AriaButtonProps} from "@react-types/button";
 
 import {forwardRef, HTMLNextUIProps} from "@nextui-org/system";
 import {Button} from "@nextui-org/button";
+import {ChevronLeftIcon, ChevronRightIcon} from "@nextui-org/shared-icons";
 
 export interface Props extends Omit<HTMLNextUIProps<"button">, keyof AriaButtonProps> {
   direction: "left" | "right";
@@ -9,39 +10,13 @@ export interface Props extends Omit<HTMLNextUIProps<"button">, keyof AriaButtonP
 
 export type NumberFieldHorizontalStepperProps = Props & AriaButtonProps;
 
-const StepperLeftIcon = () => (
-  <svg fill="none" height="10" viewBox="0 0 6 10" width="6" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M5 9L1.55482 5.70707C0.815058 5 0.815061 5 1.55482 4.29293L5 1"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="1.5"
-    />
-  </svg>
-);
-
-const StepperRightIcon = () => (
-  <svg fill="none" height="10" viewBox="0 0 6 10" width="6" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M1 9L4.44518 5.70707C5.18494 5 5.18494 5 4.44518 4.29293L1 1"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="1.5"
-    />
-  </svg>
-);
-
 const NumberFieldHorizontalStepper = forwardRef<"button", NumberFieldHorizontalStepperProps>(
   (props) => {
     const {direction, ...otherProps} = props;
 
     return (
       <Button isIconOnly {...otherProps}>
-        {direction == "left" ? <StepperLeftIcon /> : <StepperRightIcon />}
+        {direction == "left" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
       </Button>
     );
   },
--- packages/components/number-field/src/number-field-vertical-stepper.tsx
+++ packages/components/number-field/src/number-field-vertical-stepper.tsx
@@ -2,6 +2,7 @@ import type {AriaButtonProps} from "@react-types/button";
 
 import {forwardRef, HTMLNextUIProps} from "@nextui-org/system";
 import {Button} from "@nextui-org/button";
+import {ChevronUpIcon, ChevronDownIcon} from "@nextui-org/shared-icons";
 
 export interface Props extends Omit<HTMLNextUIProps<"button">, keyof AriaButtonProps> {
   direction: "up" | "down";
@@ -9,39 +10,13 @@ export interface Props extends Omit<HTMLNextUIProps<"button">, keyof AriaButtonP
 
 export type NumberFieldVerticalStepperProps = Props & AriaButtonProps;
 
-const StepperUpIcon = () => (
-  <svg fill="none" height="6" viewBox="0 0 10 6" width="10" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M9 5L5.70707 1.55482C5 0.815059 5 0.815061 4.29293 1.55482L1 5"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="1.5"
-    />
-  </svg>
-);
-
-const StepperDownIcon = () => (
-  <svg fill="none" height="6" viewBox="0 0 10 6" width="10" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M9 1L5.70707 4.44518C5 5.18494 5 5.18494 4.29293 4.44518L1 1"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      strokeWidth="1.5"
-    />
-  </svg>
-);
-
 const NumberFieldVerticalStepper = forwardRef<"button", NumberFieldVerticalStepperProps>(
   (props) => {
     const {direction, ...otherProps} = props;
 
     return (
       <Button isIconOnly {...otherProps}>
-        {direction == "up" ? <StepperUpIcon /> : <StepperDownIcon />}
+        {direction == "up" ? <ChevronUpIcon /> : <ChevronDownIcon />}
       </Button>
     );
   },
