61c991ef244fc9843862401ad95d5b83a448623e
WK Wong <wingkwong.code@gmail.com>
2024-12-28 20:47:18 +0800

    fix(number-field): typing issue on stepper buttons

--- packages/components/number-field/src/number-field-horiztonal-stepper.tsx
+++ packages/components/number-field/src/number-field-horiztonal-stepper.tsx
@@ -1,26 +1,23 @@
 import type {AriaButtonProps} from "@react-types/button";
+import type {ButtonProps} from "@nextui-org/button";
 
-import {forwardRef, HTMLNextUIProps} from "@nextui-org/system";
 import {Button} from "@nextui-org/button";
 import {ChevronLeftIcon, ChevronRightIcon} from "@nextui-org/shared-icons";
 
-export interface Props extends Omit<HTMLNextUIProps<"button">, keyof AriaButtonProps> {
+interface NumberFieldHorizontalStepperProps extends Omit<ButtonProps, keyof AriaButtonProps> {
   direction: "left" | "right";
 }
 
-export type NumberFieldHorizontalStepperProps = Props & AriaButtonProps;
-
-const NumberFieldHorizontalStepper = forwardRef<"button", NumberFieldHorizontalStepperProps>(
-  (props) => {
-    const {direction, ...otherProps} = props;
-
-    return (
-      <Button isIconOnly {...otherProps}>
-        {direction == "left" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-      </Button>
-    );
-  },
-);
+const NumberFieldHorizontalStepper = ({
+  direction,
+  ...otherProps
+}: NumberFieldHorizontalStepperProps) => {
+  return (
+    <Button isIconOnly {...otherProps}>
+      {direction === "left" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+    </Button>
+  );
+};
 
 NumberFieldHorizontalStepper.displayName = "NextUI.NumberFieldHorizontalStepper";
 
--- packages/components/number-field/src/number-field-vertical-stepper.tsx
+++ packages/components/number-field/src/number-field-vertical-stepper.tsx
@@ -1,26 +1,23 @@
 import type {AriaButtonProps} from "@react-types/button";
+import type {ButtonProps} from "@nextui-org/button";
 
-import {forwardRef, HTMLNextUIProps} from "@nextui-org/system";
 import {Button} from "@nextui-org/button";
 import {ChevronUpIcon, ChevronDownIcon} from "@nextui-org/shared-icons";
 
-export interface Props extends Omit<HTMLNextUIProps<"button">, keyof AriaButtonProps> {
+export interface NumberFieldVerticalStepperProps extends Omit<ButtonProps, keyof AriaButtonProps> {
   direction: "up" | "down";
 }
 
-export type NumberFieldVerticalStepperProps = Props & AriaButtonProps;
-
-const NumberFieldVerticalStepper = forwardRef<"button", NumberFieldVerticalStepperProps>(
-  (props) => {
-    const {direction, ...otherProps} = props;
-
-    return (
-      <Button isIconOnly {...otherProps}>
-        {direction == "up" ? <ChevronUpIcon /> : <ChevronDownIcon />}
-      </Button>
-    );
-  },
-);
+const NumberFieldVerticalStepper = ({
+  direction,
+  ...otherProps
+}: NumberFieldVerticalStepperProps) => {
+  return (
+    <Button isIconOnly {...otherProps}>
+      {direction == "up" ? <ChevronUpIcon /> : <ChevronDownIcon />}
+    </Button>
+  );
+};
 
 NumberFieldVerticalStepper.displayName = "NextUI.NumberFieldVerticalStepper";
 
