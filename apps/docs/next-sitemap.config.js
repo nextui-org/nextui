18b29d429e8fd49716ea096ed7f60614bcd1168b
Junior Garcia <jrgarciadev@gmail.com>
2025-01-05 20:50:47 -0300

    chore: cloudlfare workers removed

--- apps/docs/next-sitemap.config.js
+++ apps/docs/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://nextui.org",
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   exclude: ["/examples/*"],
 };
