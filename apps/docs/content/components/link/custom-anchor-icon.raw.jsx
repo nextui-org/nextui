57cbe995e703f09eb0446c0ed746a77d9fd1ed8f
Junior Garcia <jrgarciadev@gmail.com>
2025-01-09 16:32:50 -0300

    chore: replace frontio-ai by frontioai

--- apps/docs/content/components/link/custom-anchor-icon.raw.jsx
+++ apps/docs/content/components/link/custom-anchor-icon.raw.jsx
@@ -29,7 +29,7 @@ export default function App() {
       isExternal
       showAnchorIcon
       anchorIcon={<AnchorIcon />}
-      href="https://github.com/frontio-ai/heroui"
+      href="https://github.com/frontioai/heroui"
     >
       Custom Icon
     </Link>
--- apps/docs/content/components/link/external.raw.jsx
+++ apps/docs/content/components/link/external.raw.jsx
@@ -3,10 +3,10 @@ import {Link} from "@heroui/react";
 export default function App() {
   return (
     <div className="flex gap-4">
-      <Link isExternal href="https://github.com/frontio-ai/heroui">
+      <Link isExternal href="https://github.com/frontioai/heroui">
         External Link
       </Link>
-      <Link isExternal showAnchorIcon href="https://github.com/frontio-ai/heroui">
+      <Link isExternal showAnchorIcon href="https://github.com/frontioai/heroui">
         External Link Anchor
       </Link>
     </div>
