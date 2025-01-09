3dd9e0e248ae6cbe89eb2a02a418a62f11825585
Maharshi Alpesh <maharshialpesh@gmail.com>
2025-01-10 05:09:24 +0530

    chore: improving the swipe animations

--- packages/components/toast/src/toast.tsx
+++ packages/components/toast/src/toast.tsx
@@ -7,7 +7,7 @@ import {
   SuccessIcon,
   WarningIcon,
 } from "@nextui-org/shared-icons";
-import {AnimatePresence, motion} from "framer-motion";
+import {AnimatePresence, motion, LazyMotion, domAnimation} from "framer-motion";
 import {cloneElement, isValidElement} from "react";
 import {Spinner} from "@nextui-org/spinner";
 
@@ -98,18 +98,20 @@ const Toast = forwardRef<"div", ToastProps>((props, ref) => {
       {disableAnimation ? (
         toastContent
       ) : (
-        <AnimatePresence>
-          <motion.div {...getMotionDivProps()}>
-            <motion.div
-              animate={{opacity: 1}}
-              exit={{opacity: 0}}
-              initial={{opacity: 0}}
-              transition={{duration: 0.25, ease: "easeOut", delay: 0.1}}
-            >
-              {toastContent}
+        <LazyMotion features={domAnimation}>
+          <AnimatePresence>
+            <motion.div {...getMotionDivProps()}>
+              <motion.div
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+                initial={{opacity: 0}}
+                transition={{duration: 0.25, ease: "easeOut", delay: 0.1}}
+              >
+                {toastContent}
+              </motion.div>
             </motion.div>
-          </motion.div>
-        </AnimatePresence>
+          </AnimatePresence>
+        </LazyMotion>
       )}
     </>
   );
