69973a98e61e53a8a2d197d806552b3a42579cab
WK Wong <wingkwong.code@gmail.com>
2025-01-20 19:51:48 +0800

    fix(docs): custom impl preview for checkbox & checkbox-grp

--- apps/docs/content/components/checkbox-group/custom-implementation.raw.jsx
+++ apps/docs/content/components/checkbox-group/custom-implementation.raw.jsx
@@ -1,26 +1,26 @@
 import {useCheckbox, CheckboxGroup, Chip, VisuallyHidden, tv} from "@heroui/react";
 
-const checkbox = tv({
-  slots: {
-    base: "border-default hover:bg-default-200",
-    content: "text-default-500",
-  },
-  variants: {
-    isSelected: {
-      true: {
-        base: "border-primary bg-primary hover:bg-primary-500 hover:border-primary-500",
-        content: "text-primary-foreground pl-1",
-      },
-    },
-    isFocusVisible: {
-      true: {
-        base: "outline-none ring-2 ring-focus ring-offset-2 ring-offset-background",
-      },
-    },
-  },
-});
-
 export const CustomCheckbox = (props) => {
+  const checkbox = tv({
+    slots: {
+      base: "border-default hover:bg-default-200",
+      content: "text-default-500",
+    },
+    variants: {
+      isSelected: {
+        true: {
+          base: "border-primary bg-primary hover:bg-primary-500 hover:border-primary-500",
+          content: "text-primary-foreground pl-1",
+        },
+      },
+      isFocusVisible: {
+        true: {
+          base: "outline-none ring-2 ring-focus ring-offset-2 ring-offset-background",
+        },
+      },
+    },
+  });
+
   const {children, isSelected, isFocusVisible, getBaseProps, getLabelProps, getInputProps} =
     useCheckbox({
       ...props,
--- apps/docs/content/components/checkbox/custom-implementation.raw.jsx
+++ apps/docs/content/components/checkbox/custom-implementation.raw.jsx
@@ -20,31 +20,31 @@ export const CheckIcon = (props) => {
   );
 };
 
-const checkbox = tv({
-  slots: {
-    base: "border-default hover:bg-default-200",
-    content: "text-default-500",
-  },
-  variants: {
-    isSelected: {
-      true: {
-        base: "border-primary bg-primary hover:bg-primary-500 hover:border-primary-500",
-        content: "text-primary-foreground pl-1",
-      },
-    },
-    isFocusVisible: {
-      true: {
-        base: "outline-none ring-2 ring-focus ring-offset-2 ring-offset-background",
-      },
-    },
-  },
-});
-
 export default function App() {
   const {children, isSelected, isFocusVisible, getBaseProps, getLabelProps, getInputProps} =
     useCheckbox({
       defaultSelected: true,
     });
+
+  const checkbox = tv({
+    slots: {
+      base: "border-default hover:bg-default-200",
+      content: "text-default-500",
+    },
+    variants: {
+      isSelected: {
+        true: {
+          base: "border-primary bg-primary hover:bg-primary-500 hover:border-primary-500",
+          content: "text-primary-foreground pl-1",
+        },
+      },
+      isFocusVisible: {
+        true: {
+          base: "outline-none ring-2 ring-focus ring-offset-2 ring-offset-background",
+        },
+      },
+    },
+  });
 
   const styles = checkbox({isSelected, isFocusVisible});
 
